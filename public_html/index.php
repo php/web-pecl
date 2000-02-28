9a2bdcf32846d253fe69b37b4bf06f606a4af811
Stig Bakken <ssb@php.net>
2000-02-28 04:17:53 +0000

    don't display other menu items than documentation on the main site right now

--- public_html/index.php
+++ public_html/index.php
@@ -1,16 +1,17 @@
 <?php
 
-include("DB.php");
-
 pageHeader();
 
 print "<H1>PEAR: PHP Extension and Add-on Repository</H1>\n";
 
-print "<A HREF=\"add-package.php\">ADD NEW PACKAGE</A><BR>\n";
-print "<A HREF=\"add-author.php\">ADD NEW AUTHOR</A><BR>\n";
-print "<A HREF=\"list-packages.php\">LIST PACKAGES</A><BR>\n";
-print "<A HREF=\"list-authors.php\">LIST AUTHORS</A><BR>\n";
-print "<A HREF=\"domains.php\">DOMAIN ADMINISTRATION</A><BR>\n";
+if ($SERVER_NAME != "pear.php.net") {
+    print "<A HREF=\"add-package.php\">ADD NEW PACKAGE</A><BR>\n";
+    print "<A HREF=\"add-author.php\">ADD NEW AUTHOR</A><BR>\n";
+    print "<A HREF=\"list-packages.php\">LIST PACKAGES</A><BR>\n";
+    print "<A HREF=\"list-authors.php\">LIST AUTHORS</A><BR>\n";
+    print "<A HREF=\"domains.php\">DOMAIN ADMINISTRATION</A><BR>\n";
+}
+print "<H2><A HREF=\"doc/\">Documentation</A></H2>\n";
 
 pageFooter();
 

5fab533ac63c953cee9c3b06b9a0aa94756bf191
Rasmus Lerdorf <rasmus@php.net>
2000-08-01 01:27:47 +0000

    Hey, Colin made a logo, let's use it

--- public_html/index.php
+++ public_html/index.php
@@ -9,6 +9,7 @@ if ($SERVER_NAME != "pear.php.net" || $REMOTE_ADDR == '213.188.9.2') {
     print "<A HREF=\"packages.php\">PACKAGES</A><BR>\n";
     print "<A HREF=\"domains.php\">DOMAINS</A><BR>\n";
 }
+print "<IMG SRC=\"/gifs/pear.gif\">\n";
 print "<H2><A HREF=\"doc/\">Documentation</A></H2>\n";
 
 print "<!-- $REMOTE_ADDR -->\n";

6436e30cb6fa9bab0291da89079aaa74467845d3
Martin Jansen <mj@php.net>
2001-05-10 20:34:58 +0000

    * Added a mirrors site * Added a site with copyright information
    # Could please someone look over the license
    # stuff in copyright.php (I'm not that good
    # in license questions)?

---
+++ public_html/copyright.php
@@ -0,0 +1,54 @@
+<?php
+
+response_header("Copyright and License");
+  
+?>
+
+<H2>Copyright and Licence</H2>
+
+<H2>PHP License</H2>
+
+<P>
+For information on the PHP License (i.e. using the PHP language),
+<?php print_link('http://www.php.net/license/', 'click here'); ?>.
+</P>
+
+<H2>Website Copyright</H2>
+
+<P>
+The code, text, PHP logo, and graphical elements on this website 
+and the mirror websites (the "Site") are Copyright &copy; 2001 
+The PHP Group.  All rights reserved.
+</P>
+
+<P>
+Except as otherwise indicated elsewhere on this Site, you are free
+view, download and print the documents and information available 
+on this Site subject to the following conditions:
+</P>
+<UL>
+<LI>You may not remove any copyright or other proprietary notices
+contained in the documents and information on this Site.
+<LI>The rights granted to you constitute a license and not a transfer
+of title.
+<LI>The rights specified above to view, download and print the
+documents and information available on this Site are not applicable
+to the graphical elements, design or layout of this Site.  These
+elements of the Site are protected by trade dress and other laws
+and may not be copied or imitated in whole or in part.
+</UL>
+
+
+<P>
+For more information on the PHP Group and the PHP project, please see 
+<?php print_link('http://www.php.net/'); ?>.
+</P>
+
+<P>
+For more information on the PHP-PEAR project, please see 
+<?php print_link('http://pear.php.net/'); ?>.
+</P>
+
+<?php
+response_footer();
+?>
---
+++ public_html/mirrors.php
@@ -0,0 +1,49 @@
+<?php
+if (isset($country)) {
+  header("Location: http://$country.php.net/");
+}
+response_header("Mirrors Page");
+?>
+
+<H2>Mirror Sites</H2>
+
+<p>
+Here you can find more information about the mirrors
+of pear.php.net. Pick a mirror site close to you, or visit
+the provider's homepage:
+</p>
+
+<p>
+<table border="0" cellpadding="2" cellspacing="1" width="100%">
+ <tr bgcolor="#cccccc">
+  <th>Country</th>
+  <th>Mirror Address</th>
+  <th>Provider</th>
+  <th>Default Language</th>
+ </tr>
+<?php
+  $mprevious = 'aa';
+  foreach ($MIRRORS as $murl => $mdata) {
+    if ($mdata[4] != 1) { continue; }
+    echo '<tr bgcolor="#e0e0e0"><td>';
+    if ($mprevious != $mdata[0]) { echo $COUNTRIES[$mdata[0]]; }
+    else { echo "&nbsp;"; }
+    echo '</td><td>' . make_link($murl, $murl) .
+         '</td><td>' . make_link($mdata[3], $mdata[1]) . 
+         '</td><td>' . $LANGUAGES[$mdata[6]] . '</td></tr>';
+    $mprevious = $mdata[0];
+  }
+?>
+</table>
+</p>
+
+<!--
+<p>
+If you are interested in hosting a mirror of this site, 
+<?php print_link("/README.mirror", "here's how"); ?>.
+</p>
+//-->
+
+<?php
+response_footer();
+?>

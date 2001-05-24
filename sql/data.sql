f90260020afd56754610156d35816b163815451b
Martin Jansen <mj@php.net>
2001-05-24 11:19:47 +0000

    * added /me

--- sql/data.sql
+++ sql/data.sql
@@ -1,4 +1,5 @@
 INSERT INTO users VALUES('ssb', '738aa8d3bc02eb8712acd0eb2cf6dfd5', 'Stig S. Bakken', 'stig@php.net', 'http://www.pvv.org/~ssb/', '2001-04-18', 'ssb', NULL, 1, 1, 1, NULL);
+INSERT INTO users VALUES('mj', 'f8bc561286afb828653b05281d1b8a81', 'Martin Jansen', 'mj@php.net', 'http://martin-jansen.de/', '2001-05-24', 'mj', NULL, 1, 1, 1, NULL);
 INSERT INTO maintains VALUES('ssb','HTTP','lead');
 
 -- INSERT INTO packages (name,placeholder) VALUES('',1);

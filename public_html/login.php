19c4eeb6901de2c34308202679395807d52f1512
Martin Jansen <mj@php.net>
2001-07-05 07:32:24 +0000

    *  Login currently does not work

--- public_html/login.php
+++ public_html/login.php
@@ -1,7 +1,15 @@
-<?php
+<?php response_header("Login"); ?>
 
+<H2>Credits</H2>
+
+<P>This function has not been implemented yet.</P>
+
+<?php
+/**
 auth_require(0);
 
 header("Location: ./");
+*/
 
+response_footer();
 ?>

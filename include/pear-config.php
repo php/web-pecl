97df545f4530a4f0049abe01871a2f95f61b5190
Stig Bakken <ssb@php.net>
2001-08-02 07:10:11 +0000

    * fixed "login" function (uses mysql first, then cvs passwd file, does not   yet check cvs acls though)

--- include/pear-config.php
+++ include/pear-config.php
@@ -1,7 +1,8 @@
 <?php
 
-define('PEAR_AUTH_REALM', 'PEAR user');
+define('PEAR_AUTH_REALM', 'PEAR');
 define('PEAR_UPLOAD_TMPDIR', '/var/tmp/pear/uploads');
+define('PEAR_CVS_TMPDIR', '/var/tmp/pear/cvs');
 define('PEAR_TARBALL_DIR', '/var/lib/pear');
 define('PEAR_DATABASE_DSN', 'mysql://pear:pear@localhost/pear');
 
--- public_html/login.php
+++ public_html/login.php
@@ -1,15 +1,13 @@
-<?php response_header("Login"); ?>
-
-<H2>Login</H2>
-
-<P>This function has not been implemented yet.</P>
-
 <?php
-/**
+
 auth_require();
+$url = "http://$HTTP_HOST";
+if ($SERVER_PORT != 80) {
+    $url .= ":$SERVER_PORT";
+}
+$bn = str_replace('.', '\.', basename($PHP_SELF));
+$url .= preg_replace(":/$bn\$:", "/", $REQUEST_URI);
+header("Location: $url");
+exit;
 
-header("Location: ./");
-*/
-
-response_footer();
 ?>

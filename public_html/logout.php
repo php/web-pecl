81902b21ffff8f33d60a5896f4d1632ff12b45d1
Stig Bakken <ssb@php.net>
2001-08-07 21:17:56 +0000

    * more helpful?

--- public_html/logout.php
+++ public_html/logout.php
@@ -1,9 +1,17 @@
 <?php
 
-Header("HTTP/1.0 401 Unauthorized");
-Header("WWW-authenticate: basic realm=\"PEAR user\"");
-Header("Refresh: 1; url=\"./\"");
-
-auth_reject(PEAR_AUTH_REALM, "Logging out");
+if (isset($showmsg)) {
+    $delay = 3;
+    Header("Refresh: $delay; url=\"$PHP_SELF\"");
+    response_header("Logging Out...");
+    report_error("Press 'Cancel' when presented a new login box or ".
+		 "one saying 'authorization failed, retry?'<br />");
+    response_footer();
+} else {
+    Header("HTTP/1.0 401 Unauthorized");
+    Header("WWW-authenticate: basic realm=\"PEAR user\"");
+    Header("Refresh: 1; url=\"./\"");
+    auth_reject(PEAR_AUTH_REALM, "Logging out");
+}
 
 ?>

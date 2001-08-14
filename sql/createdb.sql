4c2cbd1dc5515c264e674e72effbd56f11f9e45a
Stig Bakken <ssb@php.net>
2001-08-14 09:27:39 +0000

    * modified to use on pair1.php.net

--- sql/createdb.sql
+++ sql/createdb.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE pear;
 
-INSERT INTO user VALUES('%.trondheim.fast.no','pear',password('pear'),'N','N','N','N','N','N','N','N','N','N','N','N','N','N');
+INSERT INTO user VALUES('pb1','pear',password('pear'),'N','N','N','N','N','N','N','N','N','N','N','N','N','N');
 INSERT INTO user VALUES('localhost','pear',password('pear'),'N','N','N','N','N','N','N','N','N','N','N','N','N','N');
 INSERT INTO db VALUES('%','pear','pear','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y');
 

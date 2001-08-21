52ca738871b0df45dcd9ecc8ce747245094c8c79
Tomas V.V.Cox <cox@php.net>
2001-08-21 02:27:59 +0000

    whitespaces

--- include/pear-auth.php
+++ include/pear-auth.php
@@ -25,38 +25,38 @@ function auth_require($admin = false)
     $auth_user =& new PEAR_User($dbh, $user);
     $ok = false;
     switch (strlen(@$auth_user->password)) {
-	// handle old-style DES-encrypted passwords
-	case 13: {
-	    $seed = substr($auth_user->password, 0, 2);
-	    if (crypt($PHP_AUTH_PW, $seed) == @$auth_user->password) {
-		$ok = true;
-	    }
-	    break;
-	}
-	// handle new-style MD5-encrypted passwords
-	case 32: {
-	    if (md5($PHP_AUTH_PW) == @$auth_user->password) {
-		$ok = true;
-	    }
-	    break;
-	}
+        // handle old-style DES-encrypted passwords
+        case 13: {
+            $seed = substr($auth_user->password, 0, 2);
+            if (crypt($PHP_AUTH_PW, $seed) == @$auth_user->password) {
+                $ok = true;
+            }
+            break;
+        }
+        // handle new-style MD5-encrypted passwords
+        case 32: {
+            if (md5($PHP_AUTH_PW) == @$auth_user->password) {
+                $ok = true;
+            }
+            break;
+        }
     }
     if (empty($auth_user->registered)) {
-	$ok = false;
+        $ok = false;
     }
     if (!$ok) {
-	if (cvs_verify_password($user, $PHP_AUTH_PW)) {
-	    $auth_user = (object)array('handle' => $user);
-	} else {
-	    auth_reject();
-	}
+        if (cvs_verify_password($user, $PHP_AUTH_PW)) {
+            $auth_user = (object)array('handle' => $user);
+        } else {
+            auth_reject();
+        }
     }
     $auth_user->_readonly = true;
     if ($admin && empty($auth_user->admin)) {
-	response_header("Insufficient Privileges");
-	report_error("Insufficient Privileges");
-	response_footer();
-	exit;
+        response_header("Insufficient Privileges");
+        report_error("Insufficient Privileges");
+        response_footer();
+        exit;
     }
 }
 
@@ -67,11 +67,11 @@ function cvs_find_password($user)
     global $cvspasswd_file;
     $fp = fopen($cvspasswd_file,"r");
     while ($line = fgets($fp, 120)) {
-	list($luser, $passwd, $groups) = explode(":", $line);
-	if ($user == $luser) {
-	    fclose($fp);
-	    return $passwd;
-	}
+        list($luser, $passwd, $groups) = explode(":", $line);
+        if ($user == $luser) {
+            fclose($fp);
+            return $passwd;
+        }
     }
     fclose($fp);
     return false;
@@ -81,9 +81,9 @@ function cvs_verify_password($user, $pass)
 {
     $psw = cvs_find_password($user);
     if (strlen($psw) > 0) {
-	if (crypt($pass,substr($psw,0,2)) == $psw) {
-	    return true;
-	}
+        if (crypt($pass,substr($psw,0,2)) == $psw) {
+            return true;
+        }
     }
     return false;
 }

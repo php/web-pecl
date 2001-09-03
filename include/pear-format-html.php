449c23bc39ef9b41e4b9c5b0aba13ad3c5e4b187
Tomas V.V.Cox <cox@php.net>
2001-09-03 02:01:21 +0000

    ws

--- include/pear-format-html.php
+++ include/pear-format-html.php
@@ -18,10 +18,10 @@ function smarty_func_page_start($params)
 {
     extract($params);
     if (empty($title)) {
-	$title = "The PHP Extension and Application Repository";
+        $title = "The PHP Extension and Application Repository";
     }
     if (isset($sidebardata)) {
-	$GLOBALS['SIDEBAR_DATA'] = $sidebardata;
+        $GLOBALS['SIDEBAR_DATA'] = $sidebardata;
     }
     response_header($title);
 }
@@ -41,20 +41,20 @@ function response_header($title = 'The PHP Extension and Application Repository'
     $_style = $style;
     $rts = rtrim($SIDEBAR_DATA);
     if (substr($rts, -1) == '-') {
-	$SIDEBAR_DATA = substr($rts, 0, -1);
+        $SIDEBAR_DATA = substr($rts, 0, -1);
     } else {
-	global $main_menu, $PHP_SELF;
-	$SIDEBAR_DATA .= "<br /><br />\n";
-	$me = basename($PHP_SELF);
-	foreach ($main_menu as $url => $tit) {
-	    $tt = str_replace(" ", "&nbsp;", $tit);
-	    if ($url == $me) {
-		$SIDEBAR_DATA .= "<b>&gt;&gt;$tt&lt;&lt;</b><br />\n";
-	    } else {
-		$SIDEBAR_DATA .= "&nbsp;&nbsp;&nbsp;<a href=\"$url\">$tt</a><br />\n";
-	    }
-	}
-	$SIDEBAR_DATA .= "<br /><br />\n";
+        global $main_menu, $PHP_SELF;
+        $SIDEBAR_DATA .= "<br /><br />\n";
+        $me = basename($PHP_SELF);
+        foreach ($main_menu as $url => $tit) {
+            $tt = str_replace(" ", "&nbsp;", $tit);
+            if ($url == $me) {
+                $SIDEBAR_DATA .= "<b>&gt;&gt;$tt&lt;&lt;</b><br />\n";
+            } else {
+                $SIDEBAR_DATA .= "&nbsp;&nbsp;&nbsp;<a href=\"$url\">$tt</a><br />\n";
+            }
+        }
+        $SIDEBAR_DATA .= "<br /><br />\n";
     }
 
     commonHeader($title);
@@ -66,11 +66,11 @@ function response_footer($style = false)
 {
     static $called;
     if ($called) {
-	return;
+        return;
     }
     $called = true;
     if (!$style) {
-	$style = $GLOBALS['_style'];
+        $style = $GLOBALS['_style'];
     }
 
     commonFooter();
@@ -123,19 +123,19 @@ function smarty_func_border_box_start($params)
 {
     extract($params);
     if (!isset($width)) {
-	$width = "90%";
+        $width = "90%";
     }
     if (!isset($indent)) {
-	$indent = "";
+        $indent = "";
     }
     print "$indent<table cellpadding=\"0\" cellspacing=\"1\" border=\"0\" width=\"$width\">\n";
     print "$indent <tr>\n";
     print "$indent  <td bgcolor=\"#000000\">\n";
     print "   <table cellpadding=\"2\" cellspacing=\"1\" border=\"0\" width=\"100%\">\n";
     if (isset($title)) {
-	print "$indent    <tr bgcolor=\"#cccccc\">\n";
-	print "$indent     <th>$title</th>\n";
-	print "$indent    </tr>\n";
+        print "$indent    <tr bgcolor=\"#cccccc\">\n";
+        print "$indent     <th>$title</th>\n";
+        print "$indent    </tr>\n";
     }
     print "$indent    <tr bgcolor=\"#ffffff\">\n";
     print "$indent     <td>\n";
@@ -145,7 +145,7 @@ function smarty_func_border_box_end($params)
 {
     extract($params);
     if (!isset($indent)) {
-	$indent = "";
+        $indent = "";
     }
     print "$indent     </td>\n";
     print "$indent    </tr>\n";
@@ -158,7 +158,7 @@ function smarty_func_border_box_end($params)
 function border_box_start($title, $width = "90%", $indent = "")
 {
     if (is_array($title)) {
-	$title = implode("</th><th>", $title);
+        $title = implode("</th><th>", $title);
     }
     print "$indent<table cellpadding=\"0\" cellspacing=\"1\" border=\"0\" width=\"$width\">\n";
     print "$indent <tr>\n";
@@ -184,7 +184,7 @@ function border_box_end($indent = "")
 function html_table_border(&$tableobj, $width = "100%")
 {
     $border = new HTML_Table('border="0" cellpadding="0" cellspacing="1" '.
-			     "width=\"{$width}\"");
+                             "width=\"{$width}\"");
     $border->addRow(array($tableobj->toHtml()), 'bgcolor="#000000"');
     print $border->toHtml(); 
 }

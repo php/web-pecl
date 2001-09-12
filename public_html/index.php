7860281791ff31416decaf88865c98159683a008
Martin Jansen <mj@php.net>
2001-09-12 10:47:43 +0000

    * we can't do much - but we can do something

--- public_html/index.php
+++ public_html/index.php
@@ -1,56 +1,26 @@
-<?php
-
-ini_set('y2k_compliance', 'on');
-if ($HTTP_SERVER_VARS['QUERY_STRING'] == 'devme') {
-    $duration = 86400 * 360;
-    setcookie('pear_dev', 'on', time() + $duration, '/');
-    $HTTP_COOKIE_VARS['pear_dev'] = 'on';
-} elseif ($HTTP_SERVER_VARS['QUERY_STRING'] == 'undevme') {
-    setcookie('pear_dev', '', time() - 3600, '/');
-    unset($HTTP_COOKIE_VARS['pear_dev']);
-}
-
-
-$SIDEBAR_DATA='
-<h3>What is PEAR?</h3>
-<p>
-
-PEAR is a framework and distribution system for reusable PHP
-components.
-
-<br />
-
-</p>
--
-';
-
-response_header();
-
-menu_link("Documentation", "http://php.net/manual/en/pear.php");
-
-if (DEVBOX) {
-    menu_link("Request PEAR Account", "account-request.php");
-    menu_link("New Package", "package-new.php");
-    menu_link("Administrators", "admin.php");
-    menu_link("Browse Packages", "packages.php");
-    menu_link("Want to contribute?", "signup.php");
-    menu_link("Upload Release", "release-upload.php");
-    $recent = get_recent_releases();
-    if (@sizeof($recent) > 0) {
-        print "<b>Recent Releases</b>\n";
-        print "<table>";
-        foreach ($recent as $release) {
-            extract($release);
-            print "<tr><td valign='top'><font size='-1'>";
-            print "<a href=\"pkginfo.php?package=$name&release=$version\">";
-            print "$name $version</a></font></td>";
-            print "<td valign='top'><font size='-1'>";
-            print "$doneby, $releasedate: $releasenotes</font></td></tr>\n";
-        }
-        print "</table>\n";
-    }
-}
-
-response_footer();
-
-?>
+<HTML>
+<HEAD>
+<TITLE>11.09.2001</TITLE>
+<META HTTP-EQUIV="Refresh" CONTENT="15; url=/index2.php">
+<link rel="stylesheet" href="/style.css">
+</HEAD>
+<BODY BGCOLOR="#000000" TEXT="#ffffff" LINK="#cccccc" ALINK="#ffffff" VLINK="#cccccc">
+<CENTER>
+<TABLE WIDTH="60%" HEIGHT="90%">
+<TR VALIGN="middle"><TD ALIGN="center">
+<P>
+<FONT SIZE="+3" STYLE="letter-spacing: 5px;"><B>11.09.2001</B></FONT>
+</P>
+<P>
+The worldwide PHP community is horrified by recent events and would like to 
+offer its deepest sympathies to the friends and families of those who lost their 
+lives this day.
+</P>
+</TD></TR>
+</TABLE>
+</CENTER>
+<P ALIGN="right">
+<FONT SIZE="-2"><A HREF="/index2.php">continue</A></FONT>
+</P>
+</BODY>
+</HTML>

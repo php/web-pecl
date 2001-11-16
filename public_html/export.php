20c257f51a8371d9632468303f2416155c95aca4
Stig Bakken <ssb@php.net>
2001-11-16 09:20:04 +0000

    * added "writers" export

--- public_html/export.php
+++ public_html/export.php
@@ -1,7 +1,12 @@
 <?php
 
+if (empty($what)) {
+	$what = basename($HTTP_SERVER_VARS['PATH_INFO']);
+}
+
+header("Content-type: text/plain");
+
 if ($what == "avail") {
-	header("Content-type: text/plain");
 	print "unavail\n";
 	$sth = $dbh->query("SELECT handle,path FROM cvs_acl");
 	while ($sth->fetchInto($row, DB_FETCHMODE_ORDERED) === DB_OK) {
@@ -12,16 +17,19 @@ if ($what == "avail") {
 		print "avail|$users|$path\n";
 	}
 } elseif ($what == "cvsusers") {
-	header("Content-type: text/plain");
 	$sth = $dbh->query("SELECT handle,name,email FROM users");
 	while ($sth->fetchInto($row, DB_FETCHMODE_ORDERED)) {
 		print implode(":", $row) . "\n";
 	}
 } elseif ($what == "passwd") {
-	header("Content-type: text/plain");
 	$sth = $dbh->query("SELECT handle,password FROM users");
 	while ($sth->fetchInto($row, DB_FETCHMODE_ORDERED)) {
 		print implode(":", $row) . ":cvs\n";
+	}
+} elseif ($what == "writers") {
+	$sth = $dbh->query("SELECT DISTINCT handle FROM cvs_acl WHERE access = 1");
+	while ($sth->fetchInto($row, DB_FETCHMODE_ORDERED)) {
+		print "{$row[0]}\n";
 	}
 }
 

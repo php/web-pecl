67b589e6b50a99522125bf2e0402cfc391f6f14f
Richard Heyes <heyesr@php.net>
2001-12-29 00:10:26 +0000

    Couple of useful debug functions

--- include/pear-debug.php
+++ include/pear-debug.php
@@ -1,9 +1,24 @@
 <?php
 
-function pre_dump($var) {
-    print "<pre>";
-    var_dump($var);
-    print "</pre>";
-}
+	function pre_dump($var) {
+	    print "<pre>";
+	    var_dump($var);
+	    print "</pre>";
+	}
 
+	// Print Dump & Die
+	function pdd($var){
+		echo '<pre>';
+		print_r($var);
+		echo '</pre>';
+		exit;
+	}
+
+	// Var Dump & Die
+	function vdd($var){
+		echo '<pre>';
+		var_dump($var);
+		echo '</pre>';
+		exit;
+	}
 ?>

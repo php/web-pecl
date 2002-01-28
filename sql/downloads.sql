a87703a2f680b949019fe9c967a4bc871939d93e
Martin Jansen <mj@php.net>
2002-01-28 16:00:18 +0000

    * Revert my patch.

--- sql/downloads.sql
+++ sql/downloads.sql
@@ -3,6 +3,8 @@ CREATE TABLE downloads (
        file	      INTEGER NOT NULL, -- REFERENCES files(id),
        package	      INTEGER NOT NULL, -- REFERENCES packages(id),
        release	      INTEGER NOT NULL, -- REFERENCES releases(id),
+       author	      INTEGER NOT NULL, -- REFERENCES users(id),
+       category       INTEGER NOT NULL, -- REFERENCES categories(id),
        dl_when	      DATETIME NOT NULL,
        dl_who	      VARCHAR(20),
        dl_host	      VARCHAR(100),

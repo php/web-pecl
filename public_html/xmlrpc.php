5a3aa1d2eb7886b2e2de75f61b51af7f8f87e630
Stig Bakken <ssb@php.net>
2002-03-30 21:55:45 +0000

    * convert php errors in the xmlrpc server to xmlrpc faults (so they   can be shown properly by the client)

--- public_html/xmlrpc.php
+++ public_html/xmlrpc.php
@@ -3,6 +3,8 @@
 if (!isset($HTTP_RAW_POST_DATA)) {
     die('invalid XML RPC request');
 }
+set_error_handler("xmlrpc_error_handler");
+
 require_once "xmlrpc-methods.php";
 $xs = xmlrpc_server_create();
 pear_register_xmlrpc_methods($xs);
@@ -11,5 +13,52 @@ $response = xmlrpc_server_call_method($xs, $HTTP_RAW_POST_DATA, null,
 header('Content-type: text/xml');
 header('Content-length: '.strlen($response));
 print $response;
+
+function xmlrpc_error_handler($errno, $errmsg, $file, $line, $vars)
+{
+    $errortype = array (
+        1   =>  "Error",
+        2   =>  "Warning",
+        4   =>  "Parsing Error",
+        8   =>  "Notice",
+        16  =>  "Core Error",
+        32  =>  "Core Warning",
+        64  =>  "Compile Error",
+        128 =>  "Compile Warning",
+        256 =>  "User Error",
+        512 =>  "User Warning",
+        1024=>  "User Notice"
+    );
+//    if ($errno & 8) return; // ignore notices
+    $prefix = $errortype[$errno];
+    $file = basename($file);
+    $error_message = "$file:$line: $prefix: $errmsg";
+    $response = "<?xml version='1.0' encoding='iso-8859-1' ?>
+<methodResponse>
+<fault>
+ <value>
+  <struct>
+   <member>
+    <name>faultString</name>
+    <value>
+     <string>$error_message</string>
+    </value>
+   </member>
+   <member>
+    <name>faultCode</name>
+    <value>
+     <int>$errno</int>
+    </value>
+   </member>
+  </struct>
+ </value>
+</fault>
+</methodResponse>
+";
+    header("Content-length: " . strlen($response));
+    header("Content-type: text/xml");
+    print $response;
+    exit;
+}
 
 ?>

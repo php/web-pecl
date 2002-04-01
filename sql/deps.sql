8410cd18ce3fb3815e749c46b52d59a29e86a51d
Stig Bakken <ssb@php.net>
2002-04-01 17:33:43 +0000

    * sanitize deps table

--- sql/deps.sql
+++ sql/deps.sql
@@ -1,10 +1,11 @@
 CREATE TABLE deps (
-  package varchar(80) NOT NULL default '',
-  release varchar(20) NOT NULL default '',
-  type varchar(6) NOT NULL default '',
-  relation varchar(6) NOT NULL default '',
-  version varchar(10) NOT NULL default '',
-  name varchar(100) NOT NULL default '',
+  package    varchar(80) NOT NULL REFERENCES packages(id),
+  release    varchar(20) NOT NULL REFERENCES releases(id),
+  type       ENUM('pkg','ext','php','prog','ldlib','rtlib','os','websrv','nsapi') DEFAULT 'has',
+  type	     varchar(6)	 NOT NULL ,
+  relation   varchar(6)	 NOT NULL,
+  version    varchar(20),
+  name	     varchar(100),
   INDEX (release),
   INDEX (package,version)
 );

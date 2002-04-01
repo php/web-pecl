e57ac9de3bc7d3223318cb4a731bca39f2265b3e
Stig Bakken <ssb@php.net>
2002-04-01 17:21:52 +0000

    * move PHP_CVS_REPO_DIR here

--- include/pear-config.php
+++ include/pear-config.php
@@ -5,5 +5,6 @@ define('PEAR_UPLOAD_TMPDIR', '/var/tmp/pear/uploads');
 define('PEAR_CVS_TMPDIR', '/var/tmp/pear/cvs');
 define('PEAR_TARBALL_DIR', '/var/lib/pear');
 define('PEAR_DATABASE_DSN', 'mysql://pear:pear@localhost/pear');
+define('PHP_CVS_REPO_DIR', '/repository/pear');
 
 ?>

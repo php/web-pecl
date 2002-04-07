01f7b5153268deb34da4dfc97c9dad71d4f8cad4
Stig Bakken <ssb@php.net>
2002-04-07 18:01:40 +0000

    * fix relation field

--- sql/deps.sql
+++ sql/deps.sql
@@ -1,9 +1,8 @@
 CREATE TABLE deps (
   package    varchar(80) NOT NULL REFERENCES packages(id),
   release    varchar(20) NOT NULL REFERENCES releases(id),
-  type       ENUM('pkg','ext','php','prog','ldlib','rtlib','os','websrv','nsapi') DEFAULT 'has',
-  type	     varchar(6)	 NOT NULL ,
-  relation   varchar(6)	 NOT NULL,
+  type       ENUM('pkg','ext','php','prog','ldlib','rtlib','os','websrv','nsapi') NOT NULL,
+  relation   ENUM('has', 'eq', 'lt', 'le', 'gt', 'ge') DEFAULT 'has',
   version    varchar(20),
   name	     varchar(100),
   INDEX (release),

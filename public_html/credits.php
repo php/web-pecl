9fdea33b08f2e4bb8e65529978ab9b721e6152af
Martin Jansen <mj@php.net>
2002-05-10 06:09:21 +0000

    * Add links to account information pages.

--- public_html/credits.php
+++ public_html/credits.php
@@ -7,18 +7,18 @@ response_header('Credits');
 <h3>PEAR website team</h3>
 
 <ul>
-  <li>Stig S&aelig;ther Bakken</li>
-  <li>Tomas V.V. Cox</li>
-  <li>Martin Jansen</li>
-  <li>Colin Viebrock</li>
+  <li><a href="/account-info.php?handle=ssb">Stig S&aelig;ther Bakken</a></li>
+  <li><a href="/account-info.php?handle=cox">Tomas V.V. Cox</a></li>
+  <li><a href="/account-info.php?handle=mj">Martin Jansen</a></li>
+  <li><a href="/account-info.php?handle=cmv">Colin Viebrock</a></li>
 </ul>
 
 <h3>PEAR documentation team</h3>
 
 <ul>
-  <li>Tomas V.V. Cox</li>
-  <li>Martin Jansen</li>
-  <li>Alexander Merz</li>
+  <li><a href="/account-info.php?handle=cox">Tomas V.V. Cox</a></li>
+  <li><a href="/account-info.php?handle=mj">Martin Jansen</a></li>
+  <li><a href="/account-info.php?handle=alexmerz">Alexander Merz</a></li>
 </ul>
 
 <small>(All in alphabetic order)</small>

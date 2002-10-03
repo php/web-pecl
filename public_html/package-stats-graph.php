81c651bd26611958d923b38a7fd8482f11bdb8b1
James Cox <imajes@php.net>
2002-10-03 18:51:18 +0000

    committing for Tal. He'll follow up with a real commit message.

--- public_html/package-stats-graph.php
+++ public_html/package-stats-graph.php
@@ -90,6 +90,7 @@
 	    $bplots[$rid]->SetFillGradient("white", $colour, GRAD_HOR);
 	    //$bplot->setFillColor("#339900");
 	    $bplots[$rid]->SetColor("black");
+	$bplots[$rid]->value->setFormat('%d'); 
         $bplots[$rid]->value->Show();
 	}
 

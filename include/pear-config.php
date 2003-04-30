f0279c2b8b7a9489ef3c493e8ffbbb5a044b5b93
Alan Knowles <alan_k@php.net>
2003-04-30 11:15:20 +0000

    dont do this late at night - and dont commit my config :)

--- include/pear-config.php
+++ include/pear-config.php
@@ -23,15 +23,15 @@ if (isset($_SERVER['PEAR_TMPDIR'])) {
     define('PEAR_CVS_TMPDIR', $_SERVER['PEAR_TMPDIR'].'/cvs');
     define('PEAR_UPLOAD_TMPDIR', $_SERVER['PEAR_TMPDIR'].'/uploads');
 } else {
-    define('PEAR_TMPDIR', '/tmp/pear');
-    define('PEAR_CVS_TMPDIR', '/tmp/pear');
-    define('PEAR_UPLOAD_TMPDIR', '/tmp/pear');
+    define('PEAR_TMPDIR', '/var/tmp/pear');
+    define('PEAR_CVS_TMPDIR', '/var/tmp/pear/cvs');
+    define('PEAR_UPLOAD_TMPDIR', '/var/tmp/pear/uploads');
 }
 
 if (isset($_SERVER['PEAR_DATABASE_DSN'])) {
     define('PEAR_DATABASE_DSN', $_SERVER['PEAR_DATABASE_DSN']);
 } else {
-    define('PEAR_DATABASE_DSN', 'mysql://alan@localhost/pear');
+    define('PEAR_DATABASE_DSN', 'mysql://pear:pear@localhost/pear'); 
 }
 if (isset($_SERVER['PEAR_AUTH_REALM'])) {
     define('PEAR_AUTH_REALM', $_SERVER['PEAR_AUTH_REALM']);
@@ -41,12 +41,12 @@ if (isset($_SERVER['PEAR_AUTH_REALM'])) {
 if (isset($_SERVER['PEAR_TARBALL_DIR'])) {
     define('PEAR_TARBALL_DIR', $_SERVER['PEAR_TARBALL_DIR']);
 } else {
-    define('PEAR_TARBALL_DIR', '/tmp/pear');
+    define('PEAR_TARBALL_DIR', '/var/lib/pear'); 
 }
 if (isset($_SERVER['PHP_CVS_REPO_DIR'])) {
     define('PHP_CVS_REPO_DIR', $_SERVER['PHP_CVS_REPO_DIR']);
 } else {
-    define('PHP_CVS_REPO_DIR', '/tmp/pear');
+    define('PHP_CVS_REPO_DIR', '/repository/pear'); 
 }
 
 ?>

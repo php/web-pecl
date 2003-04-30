9547137339cbba144d4c081a5841b68d96e5dfbd
Pierre Joye <pajoye@php.net>
2003-04-30 10:00:04 +0000

    Install a local pearweb? sure :) Typo ENT_QUOTES.

--- include/pear-manual.php
+++ include/pear-manual.php
@@ -78,7 +78,7 @@ function makeBorderTOC($this) {
         }
 
         $SIDEBAR_DATA .= '&nbsp;' . 
-            make_link($url, make_image($img, htmlspecialchars($title,EN_QUOTES,$CHARSET)) . htmlspecialchars($title,EN_QUOTES,$CHARSET) ) . 
+            make_link($url, make_image($img, htmlspecialchars($title,ENT_QUOTES,$CHARSET)) . htmlspecialchars($title,ENT_QUOTES,$CHARSET) ) . 
             '<br />';
     }
 

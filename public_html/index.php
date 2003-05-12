d49be90af2fa785942fa201775b28d665a9cbe3a
Martin Jansen <mj@php.net>
2003-05-12 19:16:02 +0000

    * Remove outdated link to the PEAR Meeting * Remove link to the Weekly News for now

--- public_html/index.php
+++ public_html/index.php
@@ -13,7 +13,7 @@
    | obtain it through the world-wide-web, please send a note to          |
    | license@php.net so we can mail you a copy immediately.               |
    +----------------------------------------------------------------------+
-   | Authors:                                                             |
+   | Authors: Martin Jansen <mj@php.net>                                  |
    +----------------------------------------------------------------------+
    $Id$
 */
@@ -52,46 +52,9 @@ href="/manual/">manual</a> for more information.
 
 response_header();
 
-?>
-<h1>PEAR Meeting in Amsterdam</h1>
-
-<div style="margin-left:2em;margin-right:2em">
-We will hold the first PEAR Meeting (following the
-<a href="http://www.phpconference.com/">Int. PHP Conference</a>) in
-Amsterdam on May 9th at 17:30.
-<br /><br />
-The <a href="http://www.terena.nl/about/secretariat/location.html">location</a>
-has been graciously sponsored by Jeroen Houben. We will provide a live
-stream from the meeting so that people that did not make it to the meeting in
-person can participate via web. Apart from that there will be the
-possibility to join the discussion via IRC.
-<br /><br />
-The agenda is as follows:
-<ol>
-<li>Quality vs. Quantity</li>
-<li>Should documentation be a requirement for new packages?
-(<?php print_link("/~mj/004-documentation.txt", "RfC"); ?>)</li>
-<li>The <acronym title="PEAR Foundation Classes">PFC</acronym>
-<a href="http://marc.theaimsgroup.com/?l=pear-dev&m=104617534710384&w=2">RfC</a></li>
-<li>PEAR on Windows</li>
-<li>Preventing conflicts among developers</li>
-<li>The website (rating system, comment system, package proposals)</li>
-<li>PEAR Installer</li>
-<li>PHP 5</li>
-<li>PEAR CS (method naming conventions?)</li>
-<li>PEAR promotion</li>
-</ol>
-
-<a href="news/meeting-2003.php">More information can be found here.</a>
-
-</div>
-<?php
-echo hdelim();
-menu_link("PEAR 1.0 has been released!", "news/release-1.0.php");
-echo hdelim();
 menu_link("Documentation", "/manual/");
+menu_link("News", "/news/");
 menu_link("Frequently Asked Questions", "/manual/en/faq.php");
-menu_link("Weekly News", "/weeklynews.php");
 menu_link("Browse Packages", "packages.php");
 menu_link("Search Packages", "package-search.php");
 menu_link("Request PEAR Account", "account-request.php");

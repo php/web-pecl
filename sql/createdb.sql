edeebcce499741e5bf818d316bf883caf5cc4fc2
Jon Parise <jon@php.net>
2003-07-26 18:49:01 +0000

    Ack, I fat-fingered my last commit; I didn't intend to change this file.

--- sql/createdb.sql
+++ sql/createdb.sql
@@ -1,6 +1,6 @@
 CREATE DATABASE pear;
 
--- INSERT INTO user (host,user,password,select_priv,insert_priv,update_priv,delete_priv,create_priv,drop_priv,reload_priv,shutdown_priv,process_priv,file_priv,grant_priv,references_priv,index_priv,alter_priv) VALUES('pb1','pear',password('pear'),'N','N','N','N','N','N','N','N','N','N','N','N','N','N');
+INSERT INTO user (host,user,password,select_priv,insert_priv,update_priv,delete_priv,create_priv,drop_priv,reload_priv,shutdown_priv,process_priv,file_priv,grant_priv,references_priv,index_priv,alter_priv) VALUES('pb1','pear',password('pear'),'N','N','N','N','N','N','N','N','N','N','N','N','N','N');
 INSERT INTO user (host,user,password,select_priv,insert_priv,update_priv,delete_priv,create_priv,drop_priv,reload_priv,shutdown_priv,process_priv,file_priv,grant_priv,references_priv,index_priv,alter_priv) VALUES('localhost','pear',password('pear'),'N','N','N','N','N','N','N','N','N','N','N','N','N','N');
 INSERT INTO db (host,db,user,select_priv,insert_priv,update_priv,delete_priv,create_priv,drop_priv,grant_priv,references_priv,index_priv,alter_priv) VALUES('%','pear','pear','Y','Y','Y','Y','Y','Y','Y','Y','Y','Y');
 

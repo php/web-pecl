11a73c2b0ed561d9d88e1947f2b7ade41ba230e3
Tomas V.V.Cox <cox@php.net>
2003-08-30 16:18:46 +0000

    Added cvs_link to the table creator script

--- sql/packages.sql
+++ sql/packages.sql
@@ -11,6 +11,7 @@ CREATE TABLE packages (
        description    TEXT,
        homepage       VARCHAR(255),
        doc_link       VARCHAR(255),
+       cvs_link       VARCHAR(255),
        PRIMARY KEY(id),
        UNIQUE INDEX(name),
        INDEX(category)

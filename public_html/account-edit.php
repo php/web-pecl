7a0e4cf47be3e17fa63a4134f8d538f1f1fdffc7
Greg Beaver <cellog@php.net>
2005-08-13 07:37:22 +0000

    sync with pearweb (indirect REST creation is enabled through user::update())

--- public_html/account-edit.php
+++ public_html/account-edit.php
@@ -3,7 +3,7 @@
    +----------------------------------------------------------------------+
    | PEAR Web site version 1.0                                            |
    +----------------------------------------------------------------------+
-   | Copyright (c) 2001-2003 The PHP Group                                |
+   | Copyright (c) 2001-2005 The PHP Group                                |
    +----------------------------------------------------------------------+
    | This source file is subject to version 2.02 of the PHP license,      |
    | that is bundled with this package in the file LICENSE, and is        |
@@ -20,18 +20,33 @@
 
 auth_require();
 
-require_once "HTML/Form.php";
+define('HTML_FORM_TH_ATTR', 'class="form-label_left"');
+define('HTML_FORM_TD_ATTR', 'class="form-input"');
+require_once 'HTML/Form.php';
 
-if (isset($HTTP_GET_VARS['handle'])) {
-    $handle = $HTTP_GET_VARS['handle'];
-} elseif (isset($HTTP_POST_VARS['handle'])) {
-    $handle = $HTTP_POST_VARS['handle'];
+if (isset($_GET['handle'])) {
+    $handle = $_GET['handle'];
+} elseif (isset($_POST['handle'])) {
+    $handle = $_POST['handle'];
 } else {
-    $handle = "";
+    $handle = false;
 }
 
+if ($handle && !ereg('^[0-9a-z_]{3,20}$', $handle)) {
+    response_header('Error:');
+    report_error("No valid handle given!");
+    response_footer();
+    exit();
+}
+
+
 ob_start();
-response_header("Edit Account: $handle");
+response_header('Edit Profile :: ' . $handle);
+
+print '<h1>Edit Profile: ';
+print '<a href="/user/'. $handle . '">' . $handle . '</a></h1>' . "\n";
+
+print "<ul><li><a href=\"#password\">Manage your password</a></li></ul>";
 
 $admin = $auth_user->isAdmin();
 $user  = $auth_user->is($handle);
@@ -42,35 +57,41 @@ if (!$admin && !$user) {
     exit();
 }
 
-if (empty($handle) && !isset($HTTP_POST_VARS['command'])) {
-    PEAR::raiseError("No valid handle found!");
+if (isset($_POST['command']) && strlen($_POST['command'] < 32)) {
+    $command = htmlspecialchars($_POST['command']);
+} else {
+    $command = 'display';
 }
 
-if (!isset($HTTP_POST_VARS['command'])) {
-    $HTTP_POST_VARS['command'] = "display";
-}
+switch ($command) {
+    case 'update':
+        $fields_list = array("name", "email", "homepage", "showemail", "userinfo", "pgpkeyid", "wishlist");
 
-switch ($HTTP_POST_VARS['command']) {
+        $user_data_post = array('handle' => $handle);
+        foreach ($fields_list as $k) {
+            if ($k == 'showemail') {
+                $user_data_post['showemail'] =  isset($_POST['showemail']) ? 1 : 0;
+                continue;
+            }
 
-    case "update" : {
+            if (!isset($_POST[$k])) {
+                report_error('Invalid data submitted.');
+                response_footer();
+                exit();
+            }
+            $user_data_post[$k] = htmlspecialchars($_POST[$k]);
+        }
 
-        $query = sprintf("UPDATE users SET name = '%s', email = '%s', homepage = '%s', userinfo = '%s', wishlist = '%s', showemail = '%s', admin = '%s'",
-                         $HTTP_POST_VARS['name'],
-                         $HTTP_POST_VARS['email'],
-                         $HTTP_POST_VARS['homepage'],
-                         addslashes($HTTP_POST_VARS['userinfo']),
-                         $HTTP_POST_VARS['wishlist'],
-                         isset($HTTP_POST_VARS['showemail']) ? 1 : 0,
-                         $admin && isset($HTTP_POST_VARS['admin']) ? 1 : 0);
-        $query .= " WHERE handle = '" . $HTTP_POST_VARS['handle'] . "'";
-        $sth = $dbh->query($query);
+        $user = user::update($user_data_post);
 
-        $old_acl = $dbh->getCol("SELECT path FROM cvs_acl ".
-                                "WHERE username = ? AND access = 1", 0,
-                                array($handle));
-        $new_acl = preg_split("/[\r\n]+/", trim($cvs_acl));
+        $old_acl = $dbh->getCol('SELECT path FROM cvs_acl '.
+                                'WHERE username = ' . "'$handle'" . ' AND access = 1', 0);
+
+        $new_acl = preg_split("/[\r\n]+/", trim($_POST['cvs_acl']));
+
         $lost_entries = array_diff($old_acl, $new_acl);
         $new_entries = array_diff($new_acl, $old_acl);
+
         if (sizeof($lost_entries) > 0) {
             $sth = $dbh->prepare("DELETE FROM cvs_acl WHERE username = ? ".
                                  "AND path = ?");
@@ -80,6 +101,7 @@ switch ($HTTP_POST_VARS['command']) {
                 $dbh->execute($sth, array($handle, $ent));
             }
         }
+
         if (sizeof($new_entries) > 0) {
             $sth = $dbh->prepare("INSERT INTO cvs_acl (username,path,access) ".
                                  "VALUES(?,?,?)");
@@ -89,187 +111,103 @@ switch ($HTTP_POST_VARS['command']) {
             }
         }
 
-        print "<i>The update has been executed successfully.</i>";
+        report_success('Your information was successfully updated.');
+        break;
 
-        print "<br /><br />";
-        print "<a href=\"account-info.php?handle=$handle\">";
-        print "[Back to info page]</a>";
-
-        $handle = $HTTP_POST_VARS['handle'];
-
-        response_footer();
-        return;
-    }
-
-    case "change_password" : {
+    case 'change_password':
         $user = &new PEAR_User($dbh, $handle);
-        $execute = true;
 
-        if (empty($_POST['password_old']) || empty($_POST['password1']) ||
+        if (empty($_POST['password_old']) || empty($_POST['password']) ||
             empty($_POST['password2'])) {
 
-            PEAR::raiseError("Please fill out all password fields.");
-            $execute = false;
+            PEAR::raiseError('Please fill out all password fields.');
+            break;
         }
 
-        if ($user->get("password") != md5($_POST['password_old'])) {
-            PEAR::raiseError("You provided a wrong old password.");
-            $execute = false;
+        if ($user->get('password') != md5($_POST['password_old'])) {
+            PEAR::raiseError('You provided a wrong old password.');
+            break;
         }
 
-        if ($_POST['password1'] != $_POST['password2']) {
-            PEAR::raiseError("The new passwords do not match.");
-            $execute = false;
+        if ($_POST['password'] != $_POST['password2']) {
+            PEAR::raiseError('The new passwords do not match.');
+            break;
         }
 
-        if ($execute === true) {
-            $user->set("password", md5($_POST['password1']));
-            if ($user->store()) {
-                auth_logout();
-                localRedirect("/login.php");
+        $user->set('password', md5($_POST['password']));
+        if ($user->store()) {
+            if (!empty($_POST['PEAR_PERSIST'])) {
+                $expire = 2147483647;
+            } else {
+                $expire = 0;
             }
+            setcookie('PEAR_PW', md5($_POST['password']), $expire, '/');
+
+            report_success('Your password was successfully updated.');
         }
-    }
-
-    default : {
-        $dbh->setFetchmode(DB_FETCHMODE_ASSOC);
-        $row = $dbh->getRow("SELECT * FROM users WHERE handle = ?",
-                            array($handle));
-        $cvs_acl_arr = $dbh->getCol("SELECT path FROM cvs_acl ".
-                                    "WHERE username = ? AND access = 1", 0,
-                                    array($handle));
-        $cvs_acl = implode("\n", $cvs_acl_arr);
-        if ($row === null) {
-            PEAR::raiseError("No account information found!");
-        }
-
-
-        print "<form action=\"" . $HTTP_SERVER_VARS['PHP_SELF'] . "?handle=" . $handle . "\" method=\"post\">\n";
-        print "<input type=\"hidden\" name=\"command\" value=\"update\" />\n";
-        print "<input type=\"hidden\" name=\"handle\" value=\"$handle\" />\n";
-
-        print "<h1>Editing account \"$handle\"</h1>\n";
-
-        print "<ul><li><a href=\"#password\">Manage your password</a></li></ul>";
-
-        $bb = new BorderBox("Edit your information");
-
-        print "<table border=\"0\" cellspacing=\"1\" cellpadding=\"5\" width=\"100%\">\n";
-        print " <tr>\n";
-        print "  <th bgcolor=\"#CCCCCC\">Handle:</th>\n";
-        print "  <td bgcolor=\"#e8e8e8\">$handle</td>\n";
-        print " </tr>\n";
-
-        print " <tr>\n";
-        print "  <th bgcolor=\"#CCCCCC\">Name:</th>\n";
-        print "  <td bgcolor=\"#e8e8e8\">";
-        HTML_Form::displayText("name", $row['name']);
-        print "  </td>\n";
-        print " </tr>\n";
-
-        print " <tr>\n";
-        print "  <th bgcolor=\"#CCCCCC\">Email:</th>\n";
-        print "  <td bgcolor=\"#e8e8e8\">";
-        HTML_Form::displayText("email", $row['email']);
-        print "  </td>\n";
-        print " </tr>\n";
-
-        print " <tr>\n";
-        print "  <th bgcolor=\"#CCCCCC\">Homepage:</th>\n";
-        print "  <td bgcolor=\"#e8e8e8\">";
-        HTML_Form::displayText("homepage", $row['homepage']);
-        print "   </td>\n";
-        print " </tr>\n";
-
-        print " <tr>\n";
-        print "  <th bgcolor=\"#CCCCCC\">Additional user information:</th>\n";
-        print "  <td bgcolor=\"#e8e8e8\">";
-        HTML_Form::displayTextarea("userinfo", $row['userinfo']);
-        print "   </td>\n";
-        print " </tr>\n";
-
-        print " <tr>\n";
-        print "  <th bgcolor=\"#CCCCCC\">CVS Access:</th>\n";
-        print "  <td bgcolor=\"#e8e8e8\">";
-        HTML_Form::displayTextarea("cvs_acl", $cvs_acl);
-        print "   </td>\n";
-        print " </tr>\n";
-
-        print " <tr>\n";
-        print "  <th bgcolor=\"#CCCCCC\">URL to wishlist:</th>\n";
-        print "  <td bgcolor=\"#e8e8e8\">";
-        HTML_Form::displayText("wishlist", $row['wishlist']);
-        print "   </td>\n";
-        print " </tr>\n";
-
-        print " <tr>\n";
-        print "  <th bgcolor=\"#CCCCCC\">Show Email address:</th>\n";
-        print "  <td bgcolor=\"#e8e8e8\">";
-        HTML_Form::displayCheckbox("showemail", $row['showemail']);
-        print "   </td>\n";
-        print " </tr>\n";
-        
-        if ($admin)    { /* show the admin checkbox only when the visitor is admin */
-            print " <tr>\n";
-            print "  <th bgcolor=\"#CCCCCC\">Administrator:</th>\n";
-            print "  <td bgcolor=\"#e8e8e8\">";
-            HTML_Form::displayCheckbox("admin", $row['admin']);
-            print "   </td>\n";
-            print " </tr>\n";
-        }  
-
-        print " <tr>\n";
-        print "  <th bgcolor=\"#CCCCCC\">&nbsp;</th>\n";
-        print "  <td bgcolor=\"#e8e8e8\"><input type=\"submit\" value=\"Submit\" name=\"submit\" />&nbsp;<input type=\"reset\" name=\"reset\" value=\"Reset\" /></td>\n";
-        print " </tr>\n";
-
-        print "</table>\n";
-
-        print "</form>\n";
-
-        $bb->end();
-
-        print "<br /><br /><a name=\"password\" />";
-
-        $bb = new BorderBox("Change password");
-
-        print "<form method=\"post\" action=\"" . $_SERVER['PHP_SELF'] . "\">\n";
-        print "<input type=\"hidden\" name=\"handle\" value=\"" . $handle . "\" />\n";
-        print "<input type=\"hidden\" name=\"command\" value=\"change_password\" />\n";
-        print "<table border=\"0\" cellspacing=\"1\" cellpadding=\"5\" width=\"100%\">\n";
-
-        print " <tr>\n";
-        print "  <th bgcolor=\"#CCCCCC\">Old Password:</th>\n";
-        print "  <td bgcolor=\"#e8e8e8\">";
-        HTML_Form::displayPassword("password_old", "", 25);
-        print "  </td>\n";
-        print " </tr>\n";
-
-        print " <tr>\n";
-        print "  <th bgcolor=\"#CCCCCC\">New Password:</th>\n";
-        print "  <td bgcolor=\"#e8e8e8\">";
-        HTML_Form::displayPassword("password1", "", 25);
-        print " repeat ";
-        HTML_Form::displayPassword("password2", "", 25);
-        print "  </td>\n";
-        print " </tr>\n";
-
-        print " <tr>\n";
-        print "  <th bgcolor=\"#CCCCCC\">&nbsp;</th>\n";
-        print "  <td bgcolor=\"#e8e8e8\"><input type=\"submit\" value=\"Submit\" name=\"submit\" />";
-        print "  (You will be redirected to a login form where you have";
-        print "  to enter your new password.)";
-        print "  </td>\n";
-        print " </tr>\n";
-
-        print "</table>\n";
-
-        $bb->end();
-
-        response_footer();
-
-    }
+        break;
 }
 
+
+$dbh->setFetchmode(DB_FETCHMODE_ASSOC);
+
+$row = $dbh->getRow('SELECT * FROM users WHERE handle = ?', array($handle));
+
+$cvs_acl_arr = $dbh->getCol('SELECT path FROM cvs_acl'
+                            . ' WHERE username = ? AND access = 1', 0,
+                            array($handle));
+$cvs_acl = implode("\n", $cvs_acl_arr);
+
+if ($row === null) {
+    error_handler($handle . ' is not a valid account name.', 'Invalid Account');
+}
+
+
+$form = new HTML_Form(htmlspecialchars($_SERVER['SCRIPT_NAME']), 'post');
+
+$form->addText('name', '<span class="accesskey">N</span>ame:',
+        $row['name'], 40, null, 'accesskey="n"');
+$form->addText('email', 'Email:',
+        $row['email'], 40, null);
+$form->addCheckbox('showemail', 'Show email address?',
+        $row['showemail']);
+$form->addText('homepage', 'Homepage:',
+        $row['homepage'], 40, null);
+$form->addText('wishlist', 'Wishlist URI:',
+        $row['wishlist'], 40, null);
+$form->addText('pgpkeyid', 'PGP Key ID:'
+        . '<p class="cell_note">(Without leading 0x)</p>',
+        $row['pgpkeyid'], 40, 20);
+$form->addTextarea('userinfo',
+        'Additional User Information:',
+        $row['userinfo'], 40, 5, null);
+$form->addTextarea('cvs_acl',
+        'CVS Access:',
+        $cvs_acl, 40, 5, null);
+$form->addSubmit('submit', 'Submit');
+$form->addHidden('handle', $handle);
+$form->addHidden('command', 'update');
+$form->display('class="form-holder" style="margin-bottom: 2em;"'
+               . ' cellspacing="1"',
+               'Edit Your Information', 'class="form-caption"');
+
+
+print '<a name="password"></a>' . "\n";
+print '<h2>&raquo; Manage your password</h2>' . "\n";
+
+$form = new HTML_Form(htmlspecialchars($_SERVER['SCRIPT_NAME']), 'post');
+$form->addPlaintext('<span class="accesskey">O</span>ld Password:',
+        $form->returnPassword('password_old', '', 40, 0,
+                              'accesskey="o"'));
+$form->addPassword('password', 'Password',
+        '', 10, null);
+$form->addCheckbox('PEAR_PERSIST', 'Remember username and password?',
+        '');
+$form->addSubmit('submit', 'Submit');
+$form->addHidden('handle', $handle);
+$form->addHidden('command', 'change_password');
+$form->display('class="form-holder" cellspacing="1"',
+               'Change Password', 'class="form-caption"');
 ob_end_flush();
+response_footer();
 ?>

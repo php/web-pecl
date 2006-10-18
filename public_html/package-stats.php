8234f37d8a3a8c2a368b770e0e18ba1f69669fdd
Greg Beaver <cellog@php.net>
2006-10-18 13:33:24 +0000

    only list PECL releases

--- public_html/package-stats.php
+++ public_html/package-stats.php
@@ -332,7 +332,8 @@ if (isset($_GET['pid']) && (int)$_GET['pid']) {
 
 	$total_packages    = number_format($dbh->getOne('SELECT COUNT(id) FROM packages WHERE package_type="pecl"'), 0, '.', ',');
 	$total_maintainers = number_format($dbh->getOne('SELECT COUNT(DISTINCT handle) FROM maintains'), 0, '.', ',');
-	$total_releases    = number_format($dbh->getOne('SELECT COUNT(*) FROM releases'), 0, '.', ',');
+	$total_releases    = number_format($dbh->getOne('SELECT COUNT(*) FROM releases r, packages p
+	                   WHERE r.package = p.id AND p.package_type="pecl"'), 0, '.', ',');
 	$total_categories  = number_format($dbh->getOne('SELECT COUNT(*) FROM categories'), 0, '.', ',');
     $total_downloads   = number_format($dbh->getOne('SELECT SUM(dl_number) FROM package_stats'), 0, '.', ',');
 	$query             = "SELECT sum(ps.dl_number) as dl_number, ps.package, ps.pid, ps.rid, ps.cid 

f79152aac6dfc799fafa61c71e0d406df182fbd9
Pierre Joye <pajoye@php.net>
2006-10-21 15:20:36 +0000

    - add sql defs for the aggr. stat

---
+++ sql/aggregated_package_stats.sql
@@ -0,0 +1,14 @@
+-- COLUMN          REFERENCES
+--
+-- package_id      packages(id)
+-- release_id      releases(id)
+
+CREATE TABLE aggregated_package_stats (
+  package_id int(11) NOT NULL default '0',
+  release_id int(11) NOT NULL default '0',
+  yearmonth date NOT NULL default '0000-00-00',
+  downloads int(10) unsigned NOT NULL default '0',
+  PRIMARY KEY  (release_id,yearmonth),
+  KEY package_id (package_id),
+  KEY downloads (downloads)
+)

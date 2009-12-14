b291be3d94da2dff88e16654a31c1353b85fc640
Pierre Joye <pajoye@php.net>
2009-12-14 09:47:41 +0000

    - drop all unused tables

---
+++ script/drop_unused_tables.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ * Drop unused tables 
+ * Author: pierre@php.net
+ */
+/* $Id$ */
+
+include __DIR__ . '/../include/pear-config.php';
+
+$drop_elections = '
+DROP TABLE IF EXISTS
+elections, election_votes_single, election_votes_multiple, election_votes_abstain, election_results, election_handle_votes, election_choices, election_account_request, zendinfo, trackbacks, apidoc_queue, tagnames, tag_package_link, `comments`, manual_notes
+';
+
+$dh = new PDO(PECL_DB_DSN, PECL_DB_USER, PECL_DB_PASSWORD);
+$res = $dh->query($sql);
+
+

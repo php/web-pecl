b9baa8b036ad813b4d032c22040ea9413e950319
Johannes Schlüter <johannes@php.net>
2010-08-06 21:40:08 +0000

    - s,CVS,SVN, (the linked page talks about SVN already)

--- public_html/doc/index.php
+++ public_html/doc/index.php
@@ -5,7 +5,7 @@ response_header("PECL specific docs");
 <h2>Some information on PECL specific topics</h2>
 
 <ul>
-<li><a href="branches.php">CVS Branches and how they relate to PECL</a></li>
+<li><a href="branches.php">SVN Branches and how they relate to PECL</a></li>
 <li><a href="http://php.net/manual/install.pecl.php">How to install a PECL extension</a></li>
 <li><a href="http://wiki.php.net/doc/scratchpad/pecldocs">How to document a PECL extension</a></li>
 </ul>

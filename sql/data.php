b02dae29728102e7d288f6b205bc2148d567893d
Ferenc Kovacs <tyrael@php.net>
2011-06-12 13:28:06 +0000

    fix for the "make create" see http://news.php.net/php.webmaster/11183

--- sql/data.php
+++ sql/data.php
@@ -1,5 +1,6 @@
 <?php
 
+require_once "../include/pear-prepend.php";
 require_once "DB.php";
 require_once "../include/pear-database.php";
 
@@ -7,7 +8,7 @@ if(!ini_get('register_globals')){
 	extract($HTTP_SERVER_VARS);
 }
 
-PEAR::setErrorHandling(PEAR_ERROR_CALLBACK, "error_handler");
+PEAR::setErrorHandling(PEAR_ERROR_CALLBACK, "data_error_handler");
 list($progname, $type, $user, $pass, $db) = $argv;
 $dbh = DB::connect("$type://$user:$pass@localhost/$db");
 $me = getenv("USER");
@@ -18,7 +19,7 @@ include "./addcategories.php";
 include "./addpackages.php";
 include "./addacls.php";
 
-function error_handler($obj) {
+function data_error_handler($obj) {
 	print "Error when adding users: ";
 	print $obj->getMessage();
 	print "\nMore info: ";

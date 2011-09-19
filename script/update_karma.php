dea321895fe7beb3619b04558cd1ff48a8fe481a
Pierre Joye <pajoye@php.net>
2011-09-19 21:18:06 +0000

    - script to migrate karma naming and remove all useless entries

---
+++ script/update_karma.php
@@ -0,0 +1,20 @@
+<?php
+/*
+ * Drop all accounts not active in any package and not having a SVN account
+ * Author: pierre@php.net
+ */
+/* $Id: cleanup_user.php 292139 2009-12-14 20:12:55Z pajoye $ */
+
+include __DIR__ . '/../include/pear-config.php';
+
+$dh = new PDO(PECL_DB_DSN, PECL_DB_USER, PECL_DB_PASSWORD);
+
+$sql = "update karma set level='developer' where level='pear.dev' or level='pecl.dev';";
+$res = $dh->query($sql);
+
+$sql = "update karma set level='admin' where level='pear.admin';";
+$res = $dh->query($sql);
+
+$sql = "delete from karma where level='pear.pepr' or level='pear.pepr' or level='pear.pepr.admin' or level='pear.doc.chm-upload' or level='pear.election' or level='pear.planet.admin' or level='pear.group' or level='pear.voter';";
+$res = $dh->query($sql);
+

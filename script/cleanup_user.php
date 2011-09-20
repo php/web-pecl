790e66731b1aeda93cb02696f0855ead86d32e72
Pierre Joye <pajoye@php.net>
2011-09-20 13:28:08 +0000

    - update cleanup script to use svn list

--- script/cleanup_user.php
+++ script/cleanup_user.php
@@ -6,20 +6,17 @@
 /* $Id$ */
 
 include __DIR__ . '/../include/pear-config.php';
-$svnusers = '/home/pierre/project/pecl/migration/svnusers';
-$svn_accounts = file($svnusers);
-function nonl(&$var) {$var = str_replace(array("\n","\r", "\r\n"), '', $var);}
-array_walk($svn_accounts, 'nonl');
+$svn_accounts = json_decode(file_get_contents(SVN_USERLIST), true);
 
-$sql = 'select handle from users  where handle NOT IN (select handle from maintains)';
 
 $dh = new PDO(PECL_DB_DSN, PECL_DB_USER, PECL_DB_PASSWORD);
 
+$sql = 'select handle from users  where handle NOT IN (select handle from maintains)';
 $res = $dh->query($sql);
 $sql_del = 'DELETE FROM users WHERE handle=';
 $del = 0;
 foreach ($res as $row) {
-	if (!in_array($row['handle'], $svn_accounts)) {
+	if (!isset($svn_accounts[$row['handle']])) {
 		$res = $dh->query($sql_del . "'" . $row['handle'] . "'");
 		if ($res) $del++;
 	}

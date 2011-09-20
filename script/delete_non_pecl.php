1ada05abe82d819f1e121eaef0bad966b3c3160c
Pierre Joye <pajoye@php.net>
2011-09-20 13:40:40 +0000

    - well, kill the package too and an option to only the DB part (no file being removed)

--- script/delete_non_pecl.php
+++ script/delete_non_pecl.php
@@ -8,6 +8,7 @@
 /* $Id$ */
 
 include __DIR__ . '/../include/pear-config.php';
+$only_db = false;
 
 // {{{ rm_rf()
 function rm_rf($path)
@@ -39,62 +40,62 @@ Please fix the permission or select a new path.\n";
     }
 }
 // }}}
-
-if (defined('PEAR_TARBALL_DIR')) {
-    $pkg_dir = PEAR_TARBALL_DIR;
-} else {
-    $pkg_dir = '/home/pierre/project/pecl/pecl-20091213/packages';
-}
-if (defined('PEAR_REST_DIR')) {
-    $rest_dir = PEAR_REST_DIR;
-} else {
-    $rest_dir = __DIR__ . '../public_html/rest';
-}
-
-if (!is_dir($pkg_dir)) {
-    echo "Invalid packages direcory $pkg_dir\n";
-    exit(1);
-}
-if (!is_dir($rest_dir)) {
-    echo "Invalid rest direcory $rest_dir\n";
-    exit(1);
-}
-if (is_dir($rest_dir) && !is_dir($rest_dir . '/p')) {
-    echo "Invalid rest direcory $rest_dir\n";
-    exit(1);
-}
-
 $dh = new PDO(PECL_DB_DSN, PECL_DB_USER, PECL_DB_PASSWORD);
-
-// Get the filenames and remove them, then delete the records
-$sql = '
-SELECT basename FROM files WHERE package IN
-(SELECT id FROM packages WHERE package_type != "pecl")
-ORDER BY basename
-';
-
-$res = $dh->query($sql);
-foreach ($res as $row) {
-    if (file_exists($pkg_dir . '/' . $row['basename'])) {
-        unlink($pkg_dir . '/' . $row['basename']);
-        echo $pkg_dir . '/' . $row['basename'] . " deleted\n";
+if (!$only_db) {
+    if (defined('PEAR_TARBALL_DIR')) {
+        $pkg_dir = PEAR_TARBALL_DIR;
+    } else {
+        $pkg_dir = '/home/pierre/project/pecl/pecl-20091213/packages';
     }
-}
-
-$sql = 'SELECT LOWER(name) as name FROM packages WHERE package_type != "pecl"';
-$res = $dh->query($sql);
-foreach ($res as $pkg) {
-    $r = $rest_dir . '/r/' . $pkg['name'];
-    $p = $rest_dir . '/p/' . $pkg['name'];
-    if (is_dir($r)) {
-        rm_rf($r);
-        echo $r . " removed\n";
-    }
-    if (is_dir($p)) {
-        rm_rf($p);
-        echo $p . " removed\n";
+    if (defined('PEAR_REST_DIR')) {
+        $rest_dir = PEAR_REST_DIR;
+    } else {
+        $rest_dir = __DIR__ . '../public_html/rest';
     }
 
+    if (!is_dir($pkg_dir)) {
+        echo "Invalid packages direcory $pkg_dir\n";
+        exit(1);
+    }
+    if (!is_dir($rest_dir)) {
+        echo "Invalid rest direcory $rest_dir\n";
+        exit(1);
+    }
+    if (is_dir($rest_dir) && !is_dir($rest_dir . '/p')) {
+        echo "Invalid rest direcory $rest_dir\n";
+        exit(1);
+    }
+
+    // Get the filenames and remove them, then delete the records
+    $sql = '
+    SELECT basename FROM files WHERE package IN
+    (SELECT id FROM packages WHERE package_type != "pecl")
+    ORDER BY basename
+    ';
+
+    $res = $dh->query($sql);
+    foreach ($res as $row) {
+        if (file_exists($pkg_dir . '/' . $row['basename'])) {
+            unlink($pkg_dir . '/' . $row['basename']);
+            echo $pkg_dir . '/' . $row['basename'] . " deleted\n";
+        }
+    }
+
+    $sql = 'SELECT LOWER(name) as name FROM packages WHERE package_type != "pecl"';
+    $res = $dh->query($sql);
+    foreach ($res as $pkg) {
+        $r = $rest_dir . '/r/' . $pkg['name'];
+        $p = $rest_dir . '/p/' . $pkg['name'];
+        if (is_dir($r)) {
+            rm_rf($r);
+            echo $r . " removed\n";
+        }
+        if (is_dir($p)) {
+            rm_rf($p);
+            echo $p . " removed\n";
+        }
+
+    }
 }
 
 $sql = '
@@ -166,3 +167,5 @@ DELETE FROM bugdb_roadmap WHERE package IN (
 ';
 $res = $dh->query($sql);
 
+$sql = 'DELETE FROM packages WHERE package_type != "pecl"';
+$res = $dh->query($sql);

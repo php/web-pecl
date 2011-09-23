e6c7126b939752753c1109f8a79610d24b0986cd
Pierre Joye <pajoye@php.net>
2011-09-23 20:33:17 +0000

    - no need of trailing /

--- public_html/index.php
+++ public_html/index.php
@@ -31,7 +31,7 @@ if (@sizeof($recent) > 0) {
         }
         $desc = htmlentities($desc);
         $RSIDEBAR_DATA .= "<tr><td valign=\"top\" class=\"compact\">";
-        $RSIDEBAR_DATA .= "<a href=\"/package/" . $name . "/\">";
+        $RSIDEBAR_DATA .= "<a href=\"/package/" . $name . "\">";
         $RSIDEBAR_DATA .= "$name $version</a><br /><i>$releasedate:</i> $desc</td></tr>";
     }
     $feed_link = "<a href=\"/feeds/\">Syndicate this</a>";

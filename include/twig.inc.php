7dc9273bb0f8a32f9e5a57505ede8532818d1249
Bruno CHALOPIN <bruno.chalopin@syspertec.com>
2012-07-26 10:26:25 +0200

    Use of composer to manage lib dependencies

--- include/twig.inc.php
+++ include/twig.inc.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../vendors/twig/lib/Twig/Autoloader.php';
+require_once __DIR__ . '/../vendor/twig/twig/lib/Twig/Autoloader.php';
 Twig_Autoloader::register();
 
 $loader = new Twig_Loader_Filesystem(realpath(__DIR__ . '/../templates/public'));

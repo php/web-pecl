b9de21800d8a6eb5750e88ac06831e9128e431b2
Bruno CHALOPIN <jedibc@php.net>
2012-07-27 14:21:06 +0200

    Added navbar fixed when scroling

--- public_html/js/pecl.js
+++ public_html/js/pecl.js
@@ -11,4 +11,30 @@ $(document).ready(function () {
             $('#frmStatistics').submit();
         });
     }
+
+// fix sub nav on scroll
+    var $win = $(window)
+        $body = $('body')
+        , $nav = $('.navbar')
+        , navTop = $('.navbar').length && $('.navbar').offset().top
+        , isFixed = 0;
+
+    processScroll();
+
+    // hack sad times - holdover until rewrite for 2.1
+    $win.on('scroll', processScroll);
+
+    function processScroll() {
+        var i, scrollTop = $win.scrollTop()
+        if (scrollTop >= navTop && !isFixed) {
+            isFixed = 1;
+            $nav.addClass('navbar-fixed-top');
+            $body.addClass('hasFixedNavbar');
+
+        } else if (scrollTop <= navTop && isFixed) {
+            isFixed = 0;
+            $nav.removeClass('navbar-fixed-top');
+            $body.removeClass('hasFixedNavbar');
+        }
+    }
 });

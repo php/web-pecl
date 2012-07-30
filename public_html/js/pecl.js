55d1bc9d7632d0bf2bfbff4e099b9b119cebccfc
Bruno Chalopin <jedibc@php.net>
2012-07-30 10:33:13 +0200

    Fixed responsive design

--- public_html/js/pecl.js
+++ public_html/js/pecl.js
@@ -12,29 +12,42 @@ $(document).ready(function () {
         });
     }
 
-// fix sub nav on scroll
-    var $win = $(window)
-        $body = $('body')
+    var $win = $(window);
+    $body = $('body')
         , $nav = $('.navbar')
         , navTop = $('.navbar').length && $('.navbar').offset().top
         , isFixed = 0;
 
-    processScroll();
+        checkResponsive();
 
-    // hack sad times - holdover until rewrite for 2.1
-    $win.on('scroll', processScroll);
+        $win.on('resize', checkResponsive);
 
-    function processScroll() {
-        var i, scrollTop = $win.scrollTop()
-        if (scrollTop >= navTop && !isFixed) {
-            isFixed = 1;
-            $nav.addClass('navbar-fixed-top');
-            $body.addClass('hasFixedNavbar');
-
-        } else if (scrollTop <= navTop && isFixed) {
-            isFixed = 0;
-            $nav.removeClass('navbar-fixed-top');
-            $body.removeClass('hasFixedNavbar');
+        function checkResponsive() {
+            if ($win.width() < 979) {
+                $body.removeClass('hasFixedNavbar');
+                $nav.addClass('navbar-fixed-top');
+                $body.addClass('hasFixedNavbarResponsive');
+                $win.off('scroll', processScroll);
+            } else {
+                $body.removeClass('navbar-fixed-top');
+                $body.removeClass('hasFixedNavbarResponsive');
+                navTop = $('.navbar').length && $('.navbar').offset().top;
+                processScroll();
+                $win.on('scroll', processScroll);
+            }
         }
-    }
+
+        function processScroll() {
+            var i, scrollTop = $win.scrollTop()
+            if (scrollTop >= navTop && !isFixed) {
+                isFixed = 1;
+                $nav.addClass('navbar-fixed-top');
+                $body.addClass('hasFixedNavbar');
+
+            } else if (scrollTop <= navTop && isFixed) {
+                isFixed = 0;
+                $nav.removeClass('navbar-fixed-top');
+                $body.removeClass('hasFixedNavbar');
+            }
+        }
 });

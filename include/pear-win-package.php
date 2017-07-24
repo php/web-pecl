78670d132109b14476cf63aacdcfd5229d841ca5
Anatol Belski <ab@php.net>
2017-07-24 11:47:55 +0200

    extend for 7.2 builds

--- include/pear-win-package.php
+++ include/pear-win-package.php
@@ -41,6 +41,10 @@ class package_dll
 
 	/* NOTE when edit here, don't forget to remove the cache file */
 	protected static $zip_name_parts = array (
+		'7.2' => array(
+			array('crt' => 'vc15', 'arch' => 'x86'),
+			array('crt' => 'vc15', 'arch' => 'x64'),
+		),
 		'7.1' => array(
 			array('crt' => 'vc14', 'arch' => 'x86'),
 			array('crt' => 'vc14', 'arch' => 'x64'),

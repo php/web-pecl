a1b275c0f4631d96b6269583d0f87dcd1eb2234c
Peter Kokot <peterkokot@gmail.com>
2018-10-30 04:35:01 +0100

    Remove some old PHP < 5 functionalities

--- src/Rest.php
+++ src/Rest.php
@@ -97,9 +97,7 @@ class Rest
  <n>' . htmlspecialchars($category['name']) . '</n>
  <c>' . PEAR_CHANNELNAME . '</c>
  <a>' . htmlspecialchars($category['name']) . '</a>
- <d>' . (version_compare(phpversion(), '5.0.0', 'lt') ?
-            utf8_encode($category['description']) :
-            $category['description']) . '</d>
+ <d>' . $category['description'] . '</d>
 </c>';
         // category info
         file_put_contents($cdir . DIRECTORY_SEPARATOR . urlencode($category['name']) .
@@ -301,12 +299,8 @@ class Rest
  <ca xlink:href="' . $extra . 'c/' . htmlspecialchars(urlencode($catinfo)) . '">' .
         htmlspecialchars($catinfo) . '</ca>
  <l>' . $package['license'] . '</l>
- <s>' . (version_compare(phpversion(), '5.0.0', 'lt') ?
-            utf8_encode($package['summary']) :
-            $package['summary']) . '</s>
- <d>' . (version_compare(phpversion(), '5.0.0', 'lt') ?
-            utf8_encode($package['description']) :
-            $package['description']) . '</d>
+ <s>' . $package['summary'] . '</s>
+ <d>' . $package['description'] . '</d>
  <r xlink:href="' . $extra . 'r/' . strtolower($package['name']) . '"/>' . $parent . $deprecated . '
 </p>';
         // package information
@@ -508,16 +502,10 @@ class Rest
  <st>' . $pkgobj->getState() . '</st>
  <l>' . $pkgobj->getLicense() . '</l>
  <m>' . $releasedby . '</m>
- <s>' . (version_compare(phpversion(), '5.0.0', 'lt') ?
-            utf8_encode(htmlspecialchars($pkgobj->getSummary())) :
-            htmlspecialchars($pkgobj->getSummary())) . '</s>
- <d>' . (version_compare(phpversion(), '5.0.0', 'lt') ?
-            utf8_encode(htmlspecialchars($pkgobj->getDescription())) :
-            htmlspecialchars($pkgobj->getDescription())) . '</d>
+ <s>' . htmlspecialchars($pkgobj->getSummary()) . '</s>
+ <d>' . htmlspecialchars($pkgobj->getDescription()) . '</d>
  <da>' . $releasedate . '</da>
- <n>' . (version_compare(phpversion(), '5.0.0', 'lt') ?
-            utf8_encode(htmlspecialchars($pkgobj->getNotes())) :
-            htmlspecialchars($pkgobj->getNotes())) . '</n>
+ <n>' . htmlspecialchars($pkgobj->getNotes()) . '</n>
  <f>' . filesize($filepath) . '</f>
  <g>http://' . PEAR_CHANNELNAME . '/get/' . $package . '-' . $pkgobj->getVersion() . '</g>
  <x xlink:href="package.' . $pkgobj->getVersion() . '.xml"/>
@@ -622,9 +610,7 @@ class Rest
     xsi:schemaLocation="http://pear.php.net/dtd/rest.maintainer
     http://pear.php.net/dtd/rest.maintainer.xsd">
  <h>' . $maintainer['handle'] . '</h>
- <n>' . (version_compare(phpversion(), '5.0.0', 'lt') ?
-            utf8_encode($maintainer['name']) :
-            $maintainer['name']) . '</n>
+ <n>' . $maintainer['name'] . '</n>
 ' . $uri . '</m>';
         // package information
         file_put_contents($mdir . DIRECTORY_SEPARATOR . $maintainer['handle'] .

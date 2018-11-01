ab11fd16040c2a229b71d8f6a2ab052913f192c6
Peter Kokot <peterkokot@gmail.com>
2018-11-01 04:11:41 +0100

    Add missing header to send_email.php template

--- templates/forms/send_email.php
+++ templates/forms/send_email.php
@@ -1,5 +1,27 @@
 <?php
 
+/*
+  +----------------------------------------------------------------------+
+  | The PECL website                                                     |
+  +----------------------------------------------------------------------+
+  | Copyright (c) 1999-2018 The PHP Group                                |
+  +----------------------------------------------------------------------+
+  | This source file is subject to version 3.01 of the PHP license,      |
+  | that is bundled with this package in the file LICENSE, and is        |
+  | available through the world-wide-web at the following url:           |
+  | https://php.net/license/3_01.txt                                     |
+  | If you did not receive a copy of the PHP license and are unable to   |
+  | obtain it through the world-wide-web, please send a note to          |
+  | license@php.net so we can mail you a copy immediately.               |
+  +----------------------------------------------------------------------+
+  | Authors:                                                             |
+  +----------------------------------------------------------------------+
+*/
+
+/**
+ * Template snippet form for sending emails.
+ */
+
 foreach ($vars as $key => $var) {
     $vars[$key] = htmlspecialchars($var, ENT_QUOTES);
 }
@@ -7,7 +29,6 @@ foreach ($vars as $key => $var) {
 ?>
 
 <form action="/account-mail.php?handle=<?= $vars['handle']; ?>" method="post" name="contact" class="pecl-form">
-
     <div>
         <label>Your name:</label>
         <input type="text" name="name" size="30" value="<?= $vars['name']; ?>" placeholder="Enter your name" required>

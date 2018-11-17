eaf3aea354db3ee44d7124731dd8d8726a99b359
Peter Kokot <peterkokot@gmail.com>
2018-11-17 20:16:04 +0100

    Migrate export.php to PDO

--- bin/export.php
+++ bin/export.php
@@ -31,29 +31,40 @@ if ($argc < 2) {
 
 $what = $argv[1];
 
-if ($what == "avail") {
-    print "unavail\n";
-    $sth = $dbh->query("SELECT username,path FROM cvs_acl");
-    while ($sth->fetchInto($row, DB_FETCHMODE_ORDERED) === DB_OK) {
-        $acl_paths[$row[1]][$row[0]] = true;
+if ($what === 'avail') {
+    echo "unavail\n";
+
+    $statement = $database->query("SELECT username, path FROM cvs_acl");
+
+    $results = [];
+
+    foreach ($statement->fetchAll() as $row) {
+        if (isset($results[$row['path']])) {
+            $results[$row['path']] .= ','.$row['username'];
+        } else {
+            $results[$row['path']] = $row['username'];
+        }
     }
-    foreach ($acl_paths as $path => $acldata) {
-        $users = implode(",", array_keys($acldata));
-        print "avail|$users|$path\n";
+
+    foreach ($results as $path => $users) {
+        echo "avail|$users|$path\n";
     }
-} elseif ($what == "cvsusers") {
-    $sth = $dbh->query("SELECT handle,name,email FROM users");
-    while ($sth->fetchInto($row, DB_FETCHMODE_ORDERED)) {
-        print implode(":", $row) . "\n";
+} elseif ($what === 'cvsusers') {
+    $statement = $database->query("SELECT handle, name, email FROM users");
+
+    foreach ($statement->fetchAll() as $row) {
+        echo implode(":", $row) . "\n";
     }
-} elseif ($what == "passwd") {
-    $sth = $dbh->query("SELECT handle,password FROM users");
-    while ($sth->fetchInto($row, DB_FETCHMODE_ORDERED)) {
-        print implode(":", $row) . ":cvs\n";
+} elseif ($what === 'passwd') {
+    $statement = $database->query("SELECT handle, password FROM users");
+
+    foreach ($statement->fetchAll() as $row) {
+        echo implode(":", $row) . ":cvs\n";
     }
-} elseif ($what == "writers") {
-    $sth = $dbh->query("SELECT DISTINCT username FROM cvs_acl WHERE access = 1");
-    while ($sth->fetchInto($row, DB_FETCHMODE_ORDERED)) {
-        print "{$row[0]}\n";
+} elseif ($what === 'writers') {
+    $statement = $database->query("SELECT DISTINCT username FROM cvs_acl WHERE access = 1");
+
+    foreach ($statement->fetchAll() as $row) {
+        echo $row['username']."\n";
     }
 }

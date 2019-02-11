df7659a780118b7f0bc2f480503277179966e4d8
Peter Kokot <peterkokot@gmail.com>
2019-02-11 03:05:36 +0100

    Move package edit page to templating

--- public_html/package-edit.php
+++ public_html/package-edit.php
@@ -22,62 +22,59 @@
  * Interface to update package information.
  */
 
-use App\BorderBox;
+use App\Auth;
 use App\Release;
+use App\Repository\PackageRepository;
+use App\Rest;
 use App\User;
 
-$release = $container->get(Release::class);
+require_once __DIR__.'/../include/pear-prepend.php';
 
-$auth->secure();
-
-response_header("Edit package");
-?>
-
-<script>
-function confirmed_goto(url, message) {
-    if (confirm(message)) {
-        location = url;
-    }
-}
-</script>
-
-<?php
-echo "<h1>Edit package</h1>";
+$container->get(Auth::class)->secure();
 
 if (!isset($_GET['id'])) {
-    PEAR::raiseError("No package ID specified.");
-    response_footer();
-    exit();
+    echo $template->render('error.php', [
+        'errors' => ['No package ID specified.'],
+    ]);
+
+    exit;
 }
 
-// The user has to be either a lead developer of the package or a PECL
-// administrator.
-$lead = User::maintains($auth_user->handle, $_GET['id'], "lead");
-$admin = $auth_user->isAdmin();
+// The user has to be either a lead developer of the package or a PECL admin.
+if (
+    !User::maintains($container->get('auth_user')->handle, $_GET['id'], 'lead')
+    && !$container->get('auth_user')->isAdmin()
+) {
+    echo $template->render('error.php', [
+        'errors' => ['Only the lead maintainer of the package or PECL administrators can edit the package.'],
+    ]);
 
-if (!$lead && !$admin) {
-    PEAR::raiseError("Only the lead maintainer of the package or PECL
-                      administrators can edit the package.");
-    response_footer();
-    exit();
+    exit;
 }
 
-// Update
+$packageRepository = $container->get(PackageRepository::class);
+$content = '';
+
 if (isset($_POST['submit'])) {
-
     if (!$_POST['name'] || !$_POST['license'] || !$_POST['summary']) {
-        PEAR::raiseError("You have to enter values for name, license and summary!");
+        echo $template->render('error.php', [
+            'errors' => ['You have to enter values for name, license and summary!'],
+        ]);
+
+        exit;
     }
 
     if (!empty($_POST['newpk_id'])) {
         $_POST['new_channel'] = 'pecl.php.net';
         $_POST['new_package'] = $database->run('SELECT name from packages WHERE id = ?', [$_POST['newpk_id']])->fetch('name');
+
         if (!$_POST['new_package']) {
             $_POST['new_channel'] = $_POST['newpk_id'] = null;
         }
     } else {
-        if ($_POST['new_channel'] == 'pecl.php.net') {
+        if ('pecl.php.net' === $_POST['new_channel']) {
             $_POST['newpk_id'] = $database->run('SELECT id from packages WHERE name = ?', [$_POST['new_package']])->fetch()['id'];
+
             if (!$_POST['newpk_id']) {
                 $_POST['new_channel'] = $_POST['new_package'] = null;
             }
@@ -88,10 +85,15 @@ if (isset($_POST['submit'])) {
     $current = $database->run('SELECT name FROM packages WHERE id = ?', [$_GET['id']])->fetch();
 
     // Check if extension name has changed and is valid
-    if ($current['name'] !== $_POST['name']
-        && !preg_match($config->get('valid_extension_name_regex'), $_POST['name'])
+    if (
+        $current['name'] !== $_POST['name']
+        && !preg_match($container->get('valid_extension_name_regex'), $_POST['name'])
     ) {
-        PEAR::raiseError('Invalid package name. PECL package names must start with a letter and preferably include only lowercase letters. Optionally, numbers and underscores are also allowed.');
+        echo $template->render('error.php', [
+            'errors' => ['Invalid package name. PECL package names must start with a letter and preferably include only lowercase letters. Optionally, numbers and underscores are also allowed.'],
+        ]);
+
+        exit;
     }
 
     $sql = 'UPDATE packages
@@ -110,7 +112,7 @@ if (isset($_POST['submit'])) {
                 newchannel = ?
             WHERE id = ?';
 
-    $qparams = [
+    $arguments = [
         $_POST['name'],
         $_POST['license'],
         $_POST['summary'],
@@ -126,179 +128,45 @@ if (isset($_POST['submit'])) {
         $_GET['id']
     ];
 
-    $statement = $database->run($sql, $qparams);
+    $database->run($sql, $arguments);
 
+    $rest = $container->get(Rest::class);
     $rest->savePackage($_POST['name']);
-    $rest->savePackagesCategory($packageEntity->info($_POST['name'], 'category'));
-    echo "<b>Package information successfully updated.</b><br /><br />\n";
-} elseif (isset($_GET['action'])) {
-    switch ($_GET['action']) {
-        case "release_remove" :
-            if (!isset($_GET['release'])) {
-                PEAR::raiseError("Missing package ID!");
-                break;
-            }
+    $rest->savePackagesCategory($packageRepository->find($_POST['name'], 'category'));
+    $content .= '<b>Package information successfully updated.</b><br><br>';
+} elseif (isset($_GET['action']) && 'release_remove' === $_GET['action']) {
+    if (!isset($_GET['release'])) {
+        echo $template->render('error.php', [
+            'errors' => ['Missing package ID!'],
+        ]);
 
-            if ($release->remove($_GET['id'], $_GET['release'])) {
-                echo "<b>Release successfully deleted.</b><br /><br />\n";
-            } else {
-                PEAR::raiseError("An error occured while deleting the release!");
-            }
+        exit;
+    }
 
-            break;
+    if ($container->get(Release::class)->remove($_GET['id'], $_GET['release'])) {
+        $content .= '<b>Release successfully deleted.</b><br><br>';
+    } else {
+        echo $template->render('error.php', [
+            'errors' => ['An error occured while deleting the release!'],
+        ]);
+
+        exit;
     }
 }
 
-$row = $packageEntity->info((int)$_GET['id']);
+$row = $packageRepository->find($_GET['id']);
 
 if (empty($row['name'])) {
-    PEAR::raiseError("Illegal package id");
-    response_footer();
+    echo $template->render('error.php', [
+        'errors' => ['Illegal package id'],
+    ]);
 
-    exit();
+    exit;
 }
 
-$bb = new BorderBox("Edit package information");
-?>
-
-<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES)?>?id=<?php echo (int)$_GET['id']; ?>" method="POST">
-<table border="0">
-<tr>
-    <td>Package name:</td>
-    <td valign="middle">
-        <input type="text" name="name" value="<?= $row['name']; ?>" size="30">
-    </td>
-</tr>
-<tr>
-    <td>License:</td>
-    <td valign="middle">
-        <input type="text" name="license" value="<?= $row['license']; ?>" size="30">
-    </td>
-</tr>
-<tr>
-    <td valign="top">Summary:</td>
-    <td>
-        <textarea name="summary" cols="40" rows="3" maxlength="255"><?= $row['summary']; ?></textarea>
-    </td>
-</tr>
-<tr>
-    <td valign="top">Description:</td>
-    <td>
-        <textarea name="description" cols="40" rows="5"><?= $row['description']; ?></textarea>
-    </td>
-</tr>
-<tr>
-    <td>Category:</td>
-    <td>
-    <select name="category" size="1">
-        <?php
-        $sth = $database->query('SELECT id, name FROM categories ORDER BY name');
-        foreach ($sth->fetchAll() as $cat_row): ?>
-        <option value="<?= $cat_row['id']; ?>" <?= (int)$row['categoryid'] == $cat_row['id'] ? ' selected' : ''; ?>><?= $cat_row['name']; ?></option>
-        <?php endforeach; ?>
-    </select>
-    </td>
-</tr>
-<tr>
-    <td>Homepage:</td>
-    <td valign="middle">
-    <input type="text" name="homepage" value="<?= $row['homepage']; ?>" size="30">
-    </td>
-</tr>
-<tr>
-    <td>Documentation:</td>
-    <td valign="middle">
-    <input type="text" name="doc_link" value="<?= $row['doc_link']; ?>" size="30">
-    </td>
-</tr>
-<tr>
-    <td>Web CVS Url:</td>
-    <td valign="middle">
-    <input type="text" name="cvs_link" value="<?= $row['cvs_link']; ?>" size="30">
-    </td>
-</tr>
-<tr>
-    <td>Bug tracker Url:</td>
-    <td valign="middle">
-    <input type="text" name="bug_link" value="<?= $row['bug_link']; ?>" size="30">
-    </td>
-</tr>
-<tr>
-    <td>Unmaintained package?</td>
-    <td valign="middle">
-        <input type="checkbox" name="unmaintained" <?= $row['unmaintained'] == 1 ? 'checked' : ''; ?>>
-    </td>
-</tr>
-<tr>
-    <td>Superseded by:</td>
-    <td valign="middle">
-        <select name="new_package" size="1">
-        <option value="" <?= $row['new_package'] == '' ? 'selected' : ''; ?>>Select package</option>
-        <?php
-        $sth = $database->query('SELECT name FROM packages WHERE package_type="pecl" ORDER BY name');
-        foreach ($sth->fetchAll() as $package): ?>
-        <option value="<?= $package['name']; ?>" <?= $row['new_package'] == $package['name'] ? 'selected' : '';?>><?= $package['name']; ?></option>
-        <?php endforeach; ?>
-        </select>
-    </td>
-</tr>
-<!-- to be enabled later, link to the wiki.php.net package page -->
-<!--
-<tr>
-    <td>Wiki:</td>
-    <td valign="middle">
-    <input type="text" name="wiki_link" value="<?= htmlspecialchars($row['wiki_link'], ENT_QUOTES); ?>" size="30">
-    </td>
-</tr>
--->
-<tr>
-    <td>New Home Link (if moved out of pecl):</td>
-    <td valign="middle">
-    <input type="text" name="new_channel" value="<?= htmlspecialchars($row['new_channel'], ENT_QUOTES); ?>" size="30">
-    </td>
-</tr>
-
-<tr>
-    <td>&nbsp;</td>
-    <td><input type="submit" name="submit" value="Save changes" />&nbsp;
-    <input type="reset" name="cancel" value="Cancel" onClick="javascript:window.location.href='/package-info.php?package=<?php echo $_GET['id']; ?>'; return false" />
-    </td>
-</tr>
-</table>
-</form>
-
-<?php
-$bb->end();
-
-echo "<br /><br />\n";
-
-$bb = new BorderBox("Manage releases");
-
-echo "<table border=\"0\">\n";
-
-echo "<tr><th>Version</th><th>Releasedate</th><th>Actions</th></tr>\n";
-
-foreach ($row['releases'] as $version => $item) {
-    echo "<tr>\n";
-    echo "  <td>" . $version . "</td>\n";
-    echo "  <td>" . $item['releasedate'] . "</td>\n";
-    echo "  <td>\n";
-
-    $url = htmlspecialchars($_SERVER['PHP_SELF'], ENT_QUOTES) . "?id=" .
-                     (int)$_GET['id'] . "&release=" .
-                     $item['id'] . "&action=release_remove";
-    $msg = "Are you sure that you want to delete the release?";
-
-    echo "<a href=\"javascript:confirmed_goto('$url', '$msg')\">"
-         . '<img src="/img/delete.gif" alt="Delete">'
-         . "</a>\n";
-
-    echo "</td>\n";
-    echo "</tr>\n";
-}
-
-echo "</table>\n";
-
-$bb->end();
-
-response_footer();
+echo $template->render('pages/package_edit.php', [
+    'content' => $content,
+    'row' => $row,
+    'categories' => $database->run('SELECT id, name FROM categories ORDER BY name')->fetchAll(),
+    'packages' => $database->run('SELECT name FROM packages WHERE package_type="pecl" ORDER BY name')->fetchAll(),
+]);
---
+++ templates/pages/package_edit.php
@@ -0,0 +1,168 @@
+<?php $this->extend('layout.php', ['title' => 'Edit package']) ?>
+
+<?php $this->start('content') ?>
+
+<script>
+function confirmed_goto(url, message) {
+    if (confirm(message)) {
+        location = url;
+    }
+}
+</script>
+
+<h1>Edit package</h1>
+
+<table cellpadding="0" cellspacing="1" style="width: 90%; border: 0px;">
+    <tr>
+        <td bgcolor="#000000">
+            <table cellpadding="2" cellspacing="1" style="width: 100%; border: 0px;">
+                <tr style="background-color: #CCCCCC;">
+                    <th>Edit package information</th>
+                </tr>
+                <tr bgcolor="#ffffff">
+                    <td>
+
+                    <form action="<?= $this->e($_SERVER['PHP_SELF']) ?>?id=<?= $this->e((int) $_GET['id']) ?>" method="post">
+<table border="0">
+<tr>
+    <td>Package name:</td>
+    <td valign="middle">
+        <input type="text" name="name" value="<?= $this->e($row['name']) ?>" size="30">
+    </td>
+</tr>
+<tr>
+    <td>License:</td>
+    <td valign="middle">
+        <input type="text" name="license" value="<?= $this->e($row['license']) ?>" size="30">
+    </td>
+</tr>
+<tr>
+    <td valign="top">Summary:</td>
+    <td>
+        <textarea name="summary" cols="40" rows="3" maxlength="255"><?= $this->e($row['summary']) ?></textarea>
+    </td>
+</tr>
+<tr>
+    <td valign="top">Description:</td>
+    <td>
+        <textarea name="description" cols="40" rows="5"><?= $this->e($row['description']) ?></textarea>
+    </td>
+</tr>
+<tr>
+    <td>Category:</td>
+    <td>
+    <select name="category" size="1">
+        <?php foreach ($categories as $cat_row): ?>
+        <option value="<?= $this->e($cat_row['id']) ?>" <?= $this->e((int) $row['categoryid'] == $cat_row['id'] ? ' selected' : '') ?>>
+            <?= $this->e($cat_row['name']) ?>
+        </option>
+        <?php endforeach ?>
+    </select>
+    </td>
+</tr>
+<tr>
+    <td>Homepage:</td>
+    <td valign="middle">
+    <input type="text" name="homepage" value="<?= $this->e($row['homepage']) ?>" size="30">
+    </td>
+</tr>
+<tr>
+    <td>Documentation:</td>
+    <td valign="middle">
+    <input type="text" name="doc_link" value="<?= $this->e($row['doc_link']) ?>" size="30">
+    </td>
+</tr>
+<tr>
+    <td>Web CVS Url:</td>
+    <td valign="middle">
+    <input type="text" name="cvs_link" value="<?= $this->e($row['cvs_link']) ?>" size="30">
+    </td>
+</tr>
+<tr>
+    <td>Bug tracker Url:</td>
+    <td valign="middle">
+    <input type="text" name="bug_link" value="<?= $this->e($row['bug_link']) ?>" size="30">
+    </td>
+</tr>
+<tr>
+    <td>Unmaintained package?</td>
+    <td valign="middle">
+        <input type="checkbox" name="unmaintained" <?= $row['unmaintained'] == 1 ? 'checked' : ''; ?>>
+    </td>
+</tr>
+<tr>
+    <td>Superseded by:</td>
+    <td valign="middle">
+        <select name="new_package" size="1">
+        <option value="" <?= $row['new_package'] == '' ? 'selected' : ''; ?>>Select package</option>
+        <?php foreach ($packages as $package): ?>
+            <option value="<?= $this->e($package['name']) ?>" <?= $row['new_package'] == $package['name'] ? 'selected' : '';?>>
+                <?= $this->e($package['name']) ?>
+            </option>
+        <?php endforeach ?>
+        </select>
+    </td>
+</tr>
+<tr>
+    <td>New Home Link (if moved out of PECL):</td>
+    <td valign="middle">
+    <input type="text" name="new_channel" value="<?= $this->e($row['new_channel']) ?>" size="30">
+    </td>
+</tr>
+
+<tr>
+    <td>&nbsp;</td>
+    <td><input type="submit" name="submit" value="Save changes" />&nbsp;
+    <input type="reset" name="cancel" value="Cancel" onClick="javascript:window.location.href='/package-info.php?package=<?= $this->e($_GET['id']) ?>'; return false">
+    </td>
+</tr>
+</table>
+</form>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+
+<br><br>
+
+<table cellpadding="0" cellspacing="1" style="width: 90%; border: 0px;">
+    <tr>
+        <td bgcolor="#000000">
+            <table cellpadding="2" cellspacing="1" style="width: 100%; border: 0px;">
+                <tr style="background-color: #CCCCCC;">
+                    <th>Manage releases</th>
+                </tr>
+                <tr bgcolor="#ffffff">
+                    <td>
+                    <table border="0">
+                        <tr>
+                            <th>Version</th>
+                            <th>Releasedate</th>
+                            <th>Actions</th>
+                        </tr>
+
+<?php foreach ($row['releases'] as $version => $item): ?>
+    <tr>
+        <td><?= $this->e($version) ?></td>
+        <td><?= $this->e($item['releasedate']) ?></td>
+        <td>
+
+        <a href="javascript:confirmed_goto('<?= $this->e($_SERVER['PHP_SELF'].'?id='.$_GET['id'].'&release='.$item['id'].'&action=release_remove') ?>', 'Are you sure that you want to delete the release?')">
+            <img src="/img/delete.gif" alt="Delete">
+        </a>
+
+        </td>
+    </tr>
+<?php endforeach ?>
+
+</table>
+                    </td>
+                </tr>
+            </table>
+        </td>
+    </tr>
+</table>
+
+<?php $this->end('content') ?>

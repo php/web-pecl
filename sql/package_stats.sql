c1cec5cbc6e38f6f2e2e1808abec93a2d9162521
Derick Rethans <github@derickrethans.nl>
2023-08-30 12:45:25 +0100

    Bump integer size

--- sql/package_stats.sql
+++ sql/package_stats.sql
@@ -5,7 +5,7 @@
 DROP TABLE IF EXISTS `package_stats`;
 
 CREATE TABLE `package_stats` (
-  `dl_number` mediumint(8) unsigned NOT NULL default '0',
+  `dl_number` int(11) unsigned NOT NULL default '0',
   `package` varchar(80) NOT NULL default '',
   `release` varchar(20) NOT NULL default '',
   `pid` int(11) NOT NULL default '0',

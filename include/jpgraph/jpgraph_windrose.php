141680d383f39e5078f0a9252c4182cd910c3f19
Derick Rethans <github@derickrethans.nl>
2023-11-15 15:20:25 +0000

    Fixed whitespace

--- include/jpgraph/jpgraph_windrose.php
+++ include/jpgraph/jpgraph_windrose.php
@@ -556,9 +556,9 @@ class WindrosePlot {
     // Alias for SetPos()
     function SetCenterPos($aX,$aY) {
         $this->iX = $aX;
-        $this->iY = $aY;        
+        $this->iY = $aY;
     }
-    
+
     function SetPos($aX,$aY) {
         $this->iX = $aX;
         $this->iY = $aY;
@@ -956,7 +956,7 @@ class WindrosePlot {
             }
 
             //$da = 360-$da;
-            
+
             if( !empty($this->iLabels[$keys[$i]]) ) {
                 $lbl = $this->iLabels[$keys[$i]];
             }

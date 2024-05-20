12970abb9dcadce597d46c38881b3ec65944ba47
Derick Rethans <github@derickrethans.nl>
2024-05-20 16:31:45 +0100

    Document new lists, and also update URLs to resources

--- templates/pages/support.php
+++ templates/pages/support.php
@@ -58,18 +58,18 @@ them have archives available, and they are also available as newsgroups on our
 <a name="subscribe"></a><h3>Subscribing and Unsubscribing</h3>
 
 <p>
-There are a variety of commands you can use to modify your subscription. Either
-send a message to pecl-<tt>whatever</tt>@lists.php.net (as in,
-pecl-dev@lists.php.net) or you can view the commands for ezmlm
-<a href="http://untroubled.org/ezmlm/ezman/ezman1.html">here</a>.
+To subscribe to pecl-dev, send an email to
+<code>pecl-dev+subscribe@lists.php.net</code> and you will be sent a
+confirmation mail that explains how to proceed with the subscription process.
+And to instead receive digested (daily) pecl-dev email, use
+<code>pecl-dev+subscribe-digest@lists.php.net</code>. Similarly, use
+<code>+unsubscribe</code> instead of <code>+subscribe</code> to do the exact opposite.
 </p>
 
 <p>
-For example, to subscribe to pecl-dev, send an email to
-pecl-dev-subscribe@lists.php.net and you will be sent a confirmation mail that
-explains how to proceed with the subscription process. And to instead receive
-digested (daily) pecl-dev email, use pecl-dev-digest-subscribe@lists.php.net.
-Similarly, use unsubscribe instead of subscribe to do the exact opposite.
+There are a variety of commands you can use to modify your subscription. Send a
+message to <code>pecl-dev-help@lists.php.net</code> to retrieve a list with
+options.
 </p>
 
 <p>
@@ -77,20 +77,15 @@ If you have questions concerning this website, you can contact
 <a href="mailto:php-webmaster@lists.php.net">php-webmaster@lists.php.net</a>.
 </p>
 
-<p>
-Of course don't forget to visit the <i>#php.pecl</i> IRC channel at the
-<a href="http://www.efnet.org">Eris Free Net</a>.
-</p>
-
 <div class="listing">
     <a name="resources"></a><h3>PECL resources</h3>
 
     <ul>
-        <li><a href="https://git.php.net/?p=php-src.git;a=blob_plain;f=CODING_STANDARDS.md;hb=HEAD">PECL/PHP Coding Standards</a></li>
+        <li><a href="https://github.com/php/php-src/blob/master/CODING_STANDARDS.md">PECL/PHP Coding Standards</a></li>
         <li><a href="https://wiki.php.net/internals/review_comments">Common issues in the proposed pecl packages</a></li>
-        <li><a href="https://php.net/internals2">PHP Internals Documentation</a></li>
+        <li><a href="https://www.phpinternalsbook.com/">PHP Internals Documentation</a></li>
         <li><a href="https://wiki.php.net/internals/references">A list of externals references about maintaining and extending PHP</a></li>
-        <li><a href="https://git.php.net/?p=php-src.git;a=blob;f=docs/parameter-parsing-api.md;hb=HEAD">Parameter Parsing API</a></li>
+        <li><a href="https://wiki.php.net/rfc/fast_zpp">Parameter Parsing API</a></li>
         <li><a href="https://wiki.php.net/internals/engine">Different information about PHP internals not yet added to the documentation</a></li>
         <li><a href="https://wiki.php.net/internals/windows">Windows specific instructions</a></li>
     </ul>

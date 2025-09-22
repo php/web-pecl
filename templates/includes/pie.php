a9246433dc6dc0ac7d7cd5b808c273b5c39c7b10
James Titcumb <james@asgrim.com>
2025-09-22 20:55:11 +0100

    Update PIE message following PECL deprecation

--- templates/includes/pie.php
+++ templates/includes/pie.php
@@ -1,8 +1,8 @@
     <div class="errors">
         <div>Have you heard about <abbr title="PHP Installer for Extensions">PIE</abbr>?</div>
         <p style="font-weight: normal;">
-            &#x1F967; PHP Installer for Extensions (PIE) is a new tool that will
-            eventually deprecate PECL. We recommend publishing your extension
+            PECL is now deprecated. &#x1F967; PHP Installer for Extensions (PIE)
+            is the replacement for PECL. We recommend publishing your extension
             using PIE instead.
         </p>
         <p>
